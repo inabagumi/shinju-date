0c17e93f34752e6d5501cbb739df6c7afaa9e2e1
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-06-03 03:55:20 +0900

    Implement pagination (#9)

--- lib/search.ts
+++ lib/search.ts
@@ -1,11 +1,14 @@
-import algoliasearch from 'algoliasearch'
+import algoliasearch, { QueryParameters } from 'algoliasearch'
 
-export default async function search<T = any>(query: string) {
+export default async function search<T = any>(
+  query: string,
+  params: QueryParameters = {}
+) {
   const client = algoliasearch(
     process.env.ALGOLIA_APPLICATION_ID!,
     process.env.ALGOLIA_API_KEY!
   )
   const index = client.initIndex(process.env.ALGOLIA_INDEX_NAME!)
 
-  return index.search<T>(query)
+  return index.search<T>({ query, hitsPerPage: 20, ...params })
 }
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,6 +1,6 @@
 import { NextContext, NextFC } from 'next'
 import Head from 'next/head'
-import React from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import SearchResults from '../components/molecules/search-results'
 import search from '../lib/search'
 import Video from '../types/video'
@@ -14,7 +14,49 @@ type Query = {
   q: string
 }
 
-const Search: NextFC<Props, Props, NextContext<Query>> = ({ hits }) => {
+const Search: NextFC<Props, Props, NextContext<Query>> = ({ hits, query }) => {
+  const targetRef = useRef<HTMLParagraphElement>(null)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [hasNext, setHasNext] = useState<boolean>(true)
+  const [page, setPage] = useState<number>(0)
+  const [results, setResults] = useState<Video[]>(hits)
+
+  useEffect(() => {
+    setHasNext(true)
+    setPage(0)
+    setResults(hits)
+  }, [hits])
+
+  useEffect(() => {
+    if (isLoading || !hasNext || !targetRef.current) return
+
+    const observer = new IntersectionObserver(entries => {
+      if (!entries.every(entry => entry.isIntersecting)) return
+
+      setIsLoading(true)
+
+      search<Video>(query, { page: page + 1 })
+        .then(response => {
+          setPage(prevPage => prevPage + 1)
+          setResults(prevResults => prevResults.concat(response.hits))
+
+          if (page >= response.nbPages) setHasNext(false)
+        })
+        .catch(() => {
+          setHasNext(false)
+        })
+        .finally(() => {
+          setIsLoading(false)
+        })
+    })
+
+    observer.observe(targetRef.current)
+
+    return () => {
+      observer.disconnect()
+    }
+  }, [targetRef.current, isLoading, hasNext, query, page])
+
   return (
     <>
       <Head>
@@ -38,13 +80,17 @@ const Search: NextFC<Props, Props, NextContext<Query>> = ({ hits }) => {
         }
       `}</style>
 
-      {hits.length > 0 ? (
-        <SearchResults values={hits} />
-      ) : (
-        <div className="notfound">
-          <p>検索結果がありません</p>
-        </div>
-      )}
+      <main>
+        {results.length > 0 ? (
+          <SearchResults values={results} />
+        ) : (
+          <div className="notfound">
+            <p>検索結果がありません</p>
+          </div>
+        )}
+
+        <p ref={targetRef} />
+      </main>
     </>
   )
 }
