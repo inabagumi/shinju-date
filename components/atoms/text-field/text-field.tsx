ff4d13707d6de02d57e8d9fbbe8f320d06e3bc6a
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-06-08 14:15:09 +0900

    Extract component of text field

---
+++ components/atoms/text-field/text-field.tsx
@@ -0,0 +1,162 @@
+import classNames from 'classnames'
+import React, {
+  ChangeEvent,
+  DetailedHTMLProps,
+  FocusEvent,
+  InputHTMLAttributes,
+  RefForwardingComponent,
+  forwardRef,
+  useCallback,
+  useState
+} from 'react'
+
+type Props = DetailedHTMLProps<
+  InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+> & {
+  icon?: string
+}
+
+type Value = string | number | string[] | undefined
+
+const TextField: RefForwardingComponent<HTMLInputElement, Props> = (
+  props,
+  ref
+) => {
+  const {
+    icon,
+    onBlur,
+    onChange,
+    onFocus,
+    type,
+    value: defaultValue,
+    ...inputProps
+  } = props
+
+  const [value, setValue] = useState<Value>(defaultValue)
+  const [isFocused, setIsFocusesd] = useState<boolean>(false)
+
+  const handleBlur = useCallback(
+    (event: FocusEvent<HTMLInputElement>) => {
+      setIsFocusesd(false)
+
+      if (typeof onBlur === 'function') onBlur(event)
+    },
+    [onBlur]
+  )
+
+  const handleChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      setValue(event.target.value)
+
+      if (typeof onChange === 'function') onChange(event)
+    },
+    [onChange]
+  )
+
+  const handleFocus = useCallback(
+    (event: FocusEvent<HTMLInputElement>) => {
+      setIsFocusesd(true)
+
+      if (typeof onFocus === 'function') onFocus(event)
+    },
+    [onFocus]
+  )
+
+  return (
+    <>
+      <div
+        className={classNames('text-field', {
+          'text-field--focused': isFocused,
+          'text-field--with-icon': icon
+        })}
+      >
+        {icon && (
+          <span aria-hidden="true" className="material-icons text-field__icon">
+            {icon}
+          </span>
+        )}
+
+        <input
+          className="text-field__input"
+          onBlur={handleBlur}
+          onChange={handleChange}
+          onFocus={handleFocus}
+          ref={ref}
+          type={type || 'text'}
+          value={value}
+          {...inputProps}
+        />
+      </div>
+
+      <style jsx>{`
+        .text-field {
+          background-color: #f5f5f5;
+          border: 1px solid #e0e0e0;
+          border-radius: 6px;
+          box-sizing: border-box;
+          color: #9e9e9e;
+          display: block;
+          max-width: 100%;
+          transition-duration: 0.4s;
+          transition-property: background-color, border-color, color;
+          transition-timing-function: ease-out;
+        }
+
+        .text-field:hover,
+        .text-field--focused {
+          border-color: #616161;
+          color: #616161;
+          transition-timing-function: ease-in;
+        }
+
+        .text-field--focused {
+          background-color: #fff;
+        }
+
+        .text-field--with-icon {
+          position: relative;
+        }
+
+        .text-field__icon {
+          align-items: center;
+          bottom: 0;
+          display: flex;
+          font-size: 20px;
+          justify-content: center;
+          overflow: hidden;
+          pointer-events: none;
+          position: absolute;
+          left: 0;
+          top: 0;
+          width: 30px;
+        }
+
+        .text-field__input {
+          appearance: none;
+          background-color: transparent;
+          border: 0;
+          box-sizing: border-box;
+          color: #616161;
+          display: block;
+          font-family: inherit;
+          font-size: 1rem;
+          height: 100%;
+          line-height: 1.5;
+          padding: 0.2rem 6px 0.3rem;
+          width: 100%;
+        }
+
+        .text-field__input:focus {
+          outline: 0;
+        }
+
+        .text-field--with-icon .text-field__input {
+          padding-left: 30px;
+        }
+      `}</style>
+    </>
+  )
+}
+
+export default forwardRef(TextField)
--- components/molecules/search-form/search-form.tsx
+++ components/molecules/search-form/search-form.tsx
@@ -4,9 +4,10 @@ import React, {
   FC,
   FormEvent,
   useCallback,
-  useState,
-  useRef
+  useRef,
+  useState
 } from 'react'
+import TextField from '../../atoms/text-field'
 
 type Props = {
   onChange?: (event: ChangeEvent<HTMLInputElement>) => void
@@ -15,116 +16,58 @@ type Props = {
 
 const SearchForm: FC<Props> = ({ onChange, query }) => {
   const textFieldRef = useRef<HTMLInputElement>(null)
-  const [value, setValue] = useState<string>(query)
   const [isFocused, setIsFocusesd] = useState<boolean>(false)
 
-  const handleChange = useCallback(
-    (event: ChangeEvent<HTMLInputElement>) => {
-      setValue(event.target.value)
-
-      if (typeof onChange === 'function') onChange(event)
-    },
-    [setValue, onChange]
-  )
+  const handleBlur = useCallback(() => {
+    setIsFocusesd(false)
+  }, [])
 
   const handleSubmit = useCallback(
     (event: FormEvent<HTMLFormElement>) => {
       event.preventDefault()
 
-      if (textFieldRef.current && isFocused) textFieldRef.current.blur()
+      if (textFieldRef.current) textFieldRef.current.blur()
     },
-    [isFocused, textFieldRef.current]
+    [textFieldRef.current]
   )
+
+  const handleFocus = useCallback(() => {
+    setIsFocusesd(true)
+  }, [])
 
   return (
     <>
+      <form
+        action="/search"
+        className={classNames('search-form', {
+          'search-form--focused': isFocused
+        })}
+        method="get"
+        onSubmit={handleSubmit}
+      >
+        <TextField
+          aria-label="検索"
+          icon="search"
+          onBlur={handleBlur}
+          onChange={onChange}
+          onFocus={handleFocus}
+          ref={textFieldRef}
+          value={query}
+        />
+      </form>
+
       <style jsx>{`
-        .text-field {
-          align-items: center;
-          background-color: #f5f5f5;
-          border: 1px solid #e0e0e0;
-          border-radius: 6px;
-          box-sizing: border-box;
-          color: #9e9e9e;
-          display: flex;
-          max-width: 100%;
-          position: relative;
-          transition-duration: 0.3s;
-          transition-property: background-color, border-color, color, width;
-          transition-timing-function: ease-out;
+        .search-form {
+          margin: 0;
+          transition: width 0.3s ease-out;
           width: 240px;
         }
 
-        .text-field:hover,
-        .text-field--focused {
-          border-color: #616161;
-          color: #616161;
+        .search-form--focused {
           transition-timing-function: ease-in;
-        }
-
-        .text-field--focused {
-          background-color: #fff;
           width: 320px;
         }
-
-        .text-field__label {
-          display: block;
-          left: 6px;
-          overflow: hidden;
-          position: absolute;
-        }
-
-        .text-field__icon {
-          display: block;
-          font-size: 20px;
-          pointer-events: none;
-        }
-
-        .text-field__input {
-          appearance: none;
-          background-color: transparent;
-          border: 0;
-          box-sizing: border-box;
-          color: #616161;
-          display: block;
-          font-family: inherit;
-          font-size: 1rem;
-          height: 100%;
-          line-height: 1.5;
-          padding: 0.25rem 6px 0.25rem 30px;
-          width: 100%;
-        }
-
-        .text-field__input:focus {
-          outline: 0;
-        }
       `}</style>
-
-      <form action="/search" method="get" onSubmit={handleSubmit}>
-        <div
-          className={classNames('text-field', {
-            'text-field--focused': isFocused
-          })}
-        >
-          <label className="text-field__label" htmlFor="search-query">
-            <span aria-label="検索" className="material-icons text-field__icon">
-              search
-            </span>
-          </label>
-
-          <input
-            className="text-field__input"
-            id="search-query"
-            name="q"
-            type="search"
-            onBlur={() => setIsFocusesd(false)}
-            onChange={handleChange}
-            onFocus={() => setIsFocusesd(true)}
-            ref={textFieldRef}
-            value={value}
-          />
-        </div>
-      </form>
     </>
   )
 }
