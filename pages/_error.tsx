e0518496ec22a5290e93777ead36abc871af32f4
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-06-09 03:03:29 +0900

    improve error handling

---
+++ pages/_error.tsx
@@ -0,0 +1,36 @@
+import Error from 'next/error'
+import Head from 'next/head'
+import React from 'react'
+
+export default class extends Error {
+  render() {
+    const { statusCode } = this.props
+
+    return (
+      <>
+        <Head>
+          <title>{statusCode} - あにまーれサーチ</title>
+        </Head>
+
+        <div className="error">
+          <h2 className="error__title">{statusCode}</h2>
+        </div>
+
+        <style jsx>{`
+          .error {
+            align-items: center;
+            display: flex;
+            height: 100%;
+            justify-content: center;
+          }
+
+          .error__title {
+            color: #212121;
+            font-size: 5rem;
+            font-weight: 700;
+          }
+        `}</style>
+      </>
+    )
+  }
+}
