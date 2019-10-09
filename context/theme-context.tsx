3da6a9e2454a9190c56dab0cc6e59824d8d7d3ce
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-10-10 00:44:08 +0900

    feat: replace to localForage (#354)

--- context/theme-context.tsx
+++ context/theme-context.tsx
@@ -1,3 +1,4 @@
+import localForage from 'localforage'
 import React, {
   FC,
   ReactElement,
@@ -11,59 +12,46 @@ export const ThemeContext = React.createContext({
   toggleTheme: () => {}
 })
 
-const getCurrentTheme = (): string | null => {
-  let theme: string | null = null
-
-  try {
-    theme = localStorage.getItem('theme')
-  } catch {} // eslint-disable-line no-empty
-
-  return theme
-}
-
 export const ThemeProvider: FC = ({ children }): ReactElement => {
   const [theme, setTheme] = useState<string>('')
 
   const toggleTheme = useCallback((): void => {
-    setTheme(theme => {
-      const nextTheme = theme !== 'dark' ? 'dark' : ''
+    const nextTheme = theme !== 'dark' ? 'dark' : ''
 
-      try {
-        localStorage.setItem('theme', nextTheme)
-      } catch (error) {
-        console.error(error)
-      }
+    localForage.setItem<string>('theme', nextTheme)
 
-      return nextTheme
-    })
-  }, [])
+    setTheme(nextTheme)
+  }, [theme])
 
   const handlePrefersColorSchemeChange = useCallback(
     (event: MediaQueryListEvent): void => {
-      const theme = event.matches ? 'dark' : ''
+      const nextTheme = event.matches ? 'dark' : ''
 
-      try {
-        localStorage.setItem('theme', theme)
-      } catch (error) {
-        console.error(error)
-      }
+      localForage.setItem<string>('theme', nextTheme)
 
-      setTheme(theme)
+      setTheme(nextTheme)
     },
     []
   )
 
   useEffect(() => {
-    const currentTheme = getCurrentTheme()
     const mediaQueryList = matchMedia('(prefers-color-scheme: dark)')
 
-    setTheme(
-      typeof currentTheme === 'string'
-        ? currentTheme
-        : mediaQueryList.matches
-        ? 'dark'
-        : ''
-    )
+    localForage
+      .getItem<string>('theme')
+      .then(value => {
+        const nextTheme =
+          typeof value !== 'string'
+            ? mediaQueryList.matches
+              ? 'dark'
+              : ''
+            : value
+
+        setTheme(nextTheme)
+      })
+      .catch(() => {
+        setTheme(mediaQueryList.matches ? 'dark' : '')
+      })
 
     mediaQueryList.addListener(handlePrefersColorSchemeChange)
 
