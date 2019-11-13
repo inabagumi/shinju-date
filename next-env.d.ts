8e27b495bd16f6a441e144ab2d1d096113d388c6
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-11-13 18:15:25 +0900

    fix: hasNext (#448)

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,7 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 
-declare module '@formatjs/intl-pluralrules/dist/locale-data/ja'
-declare module '@formatjs/intl-pluralrules/polyfill'
 declare module '@formatjs/intl-relativetimeformat/dist/locale-data/ja'
 declare module '@formatjs/intl-relativetimeformat/polyfill'
--- src/components/molecules/search/search.tsx
+++ src/components/molecules/search/search.tsx
@@ -1,12 +1,7 @@
+import classNames from 'classnames'
 import chunk from 'lodash/chunk'
-import React, {
-  FC,
-  ReactElement,
-  useCallback,
-  useEffect,
-  useState
-} from 'react'
-import { InView } from 'react-intersection-observer'
+import React, { FC, useEffect, useState } from 'react'
+import { useInView } from 'react-intersection-observer'
 import useSearch from '../../../hooks/use-search'
 import Video from '../../../types/video'
 import Spinner from '../../atoms/spinner'
@@ -18,45 +13,49 @@ interface Props {
   query: string
 }
 
-const Search: FC<Props> = ({ query }): ReactElement => {
+const Search: FC<Props> = ({ query }): JSX.Element => {
   const [page, setPage] = useState(0)
-  const { hasNext, items } = useSearch<Video>(query, page)
+  const { hasNext, isLoading, items } = useSearch<Video>(query, page)
+  const [loadingRef, inView] = useInView()
 
-  useEffect((): void => {
+  useEffect(() => {
     setPage(0)
   }, [query])
 
-  const handleChange = useCallback(() => {
-    setPage((page): number => page + 1)
-  }, [])
+  useEffect(() => {
+    if (!inView || isLoading) return
+
+    setPage(page => page + 1)
+  }, [inView, isLoading])
 
   return (
     <>
       <div className="margin-top--lg">
-        {chunk(items, COLUMNS_COUNT).map(
-          (row, i): ReactElement => (
-            <div className="row" key={`row-${i}`}>
-              {row.map(
-                (value): ReactElement => (
-                  <div
-                    className={`col col--${12 /
-                      COLUMNS_COUNT} padding-bottom--lg padding-horiz--sm`}
-                    key={value.url}
-                  >
-                    <VideoCard value={value} />
-                  </div>
-                )
-              )}
-            </div>
-          )
-        )}
+        {chunk(items, COLUMNS_COUNT).map<JSX.Element>((row, i) => (
+          <div className="row" key={`row-${i}`}>
+            {row.map<JSX.Element>(value => {
+              const className = classNames(
+                'col',
+                `col--${12 / COLUMNS_COUNT}`,
+                'padding-bottom--lg',
+                'padding-horiz--sm'
+              )
+
+              return (
+                <div className={className} key={value.url}>
+                  <VideoCard value={value} />
+                </div>
+              )
+            })}
+          </div>
+        ))}
       </div>
 
       <footer className="search__footer">
         {hasNext && (
-          <InView as="div" className="search__loading" onChange={handleChange}>
+          <div className="search__loading" ref={loadingRef}>
             <Spinner aria-label="読み込み中..." />
-          </InView>
+          </div>
         )}
       </footer>
 
@@ -65,7 +64,7 @@ const Search: FC<Props> = ({ query }): ReactElement => {
           padding: 2rem 0;
         }
 
-        :global(.search__loading) {
+        .search__loading {
           align-items: center;
           display: flex;
           height: 100%;
