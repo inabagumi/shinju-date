dd817c406e276547e3259765c7b797466f7c38f6
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-05-06 10:37:34 +0900

    perf: stop relative time refresh when off screen

--- src/components/atoms/time/time.tsx
+++ src/components/atoms/time/time.tsx
@@ -1,6 +1,7 @@
 import { format, formatDistanceToNow } from 'date-fns'
 import jaLocale from 'date-fns/locale/ja'
 import React, { FC, useEffect, useState } from 'react'
+import { useInView } from 'react-intersection-observer'
 
 const relativeTime = (date: Date): string =>
   formatDistanceToNow(date, {
@@ -15,8 +16,11 @@ type Props = {
 
 const Time: FC<Props> = ({ date }) => {
   const [label, setLabel] = useState(() => relativeTime(date))
+  const [timeRef, inView] = useInView()
 
   useEffect(() => {
+    if (!inView) return
+
     const timerID = setInterval(() => {
       setLabel(relativeTime(date))
     }, 5000)
@@ -24,11 +28,12 @@ const Time: FC<Props> = ({ date }) => {
     return (): void => {
       clearInterval(timerID)
     }
-  }, [date])
+  }, [date, inView])
 
   return (
     <time
       dateTime={date.toISOString()}
+      ref={timeRef}
       title={format(date, 'yyyy/MM/dd HH:mm:ss')}
     >
       {label}
