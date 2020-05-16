973f4d5afb64a76f16d119e2531a769b000c91e8
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-05-17 00:23:06 +0900

    feat(layout): always hide sidebar

--- src/components/organisms/header/moon.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC, ReactElement } from 'react'
-import Emoji from '../../atoms/emoji'
-
-const Moon: FC = (): ReactElement => (
-  <Emoji label="ダークモードオン" value="🌙" />
-)
-
-export default Moon
--- src/components/organisms/header/sun.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC, ReactElement } from 'react'
-import Emoji from '../../atoms/emoji'
-
-const Sun: FC = (): ReactElement => (
-  <Emoji label="ダークモードオフ" value="☀️️" />
-)
-
-export default Sun
