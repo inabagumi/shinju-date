cd63a885f83dd6cbf2972d95486767b3f7d1dbcc
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-05-20 20:28:03 +0900

    fix(card): reflect time changes on badges

--- src/components/molecules/VideoCard/VideoCard.tsx
+++ src/components/molecules/VideoCard/VideoCard.tsx
@@ -1,6 +1,13 @@
 import clsx from 'clsx'
-import { isPast, parseJSON } from 'date-fns'
-import React, { DetailedHTMLProps, FC, HTMLAttributes } from 'react'
+import { isBefore, parseJSON } from 'date-fns'
+import React, {
+  DetailedHTMLProps,
+  FC,
+  HTMLAttributes,
+  useEffect,
+  useState
+} from 'react'
+import { useInView } from 'react-intersection-observer'
 
 import Skeleton from '@/components/atoms/Skeleton'
 import Time from '@/components/atoms/Time'
@@ -22,54 +29,70 @@ type Props = DetailedHTMLProps<
   value?: Video
 }
 
-const VideoCard: FC<Props> = ({ timeOptions, value, ...props }) => (
-  <a
-    className={clsx('card', styles.video)}
-    href={value?.url}
-    rel="noopener noreferrer"
-    target="_blank"
-    {...props}
-  >
-    <div className={clsx('card__image', styles.image)}>
-      {value ? (
-        <>
+const VideoCard: FC<Props> = ({ timeOptions, value, ...props }) => {
+  const [now, setNow] = useState(() => new Date())
+  const [imageRef, inView] = useInView()
+
+  useEffect(() => {
+    if (!value || !inView) return
+
+    const timerID = setInterval(() => {
+      setNow(new Date())
+    })
+
+    return (): void => {
+      clearInterval(timerID)
+    }
+  }, [value, inView])
+
+  return (
+    <a
+      className={clsx('card', styles.video)}
+      href={value?.url}
+      rel="noopener noreferrer"
+      target="_blank"
+      {...props}
+    >
+      <div className={clsx('card__image', styles.image)} ref={imageRef}>
+        {value ? (
           <Thumbnail id={value.id} />
+        ) : (
+          <Skeleton className={styles.thumbnailSkeleton} variant="rect" />
+        )}
 
-          {!isZeroSeconds(parseDuration(value.duration)) ? (
-            <Time
-              className={clsx('badge', 'badge--info', styles.duration)}
-              dateTime={value.duration}
-              variant="duration"
-            />
-          ) : isPast(parseJSON(value.publishedAt)) ? (
-            <span className={clsx('badge', 'badge--info', styles.liveNow)}>
-              ライブ配信中
-            </span>
-          ) : null}
-        </>
-      ) : (
-        <Skeleton className={styles.thumbnailSkeleton} variant="rect" />
-      )}
-    </div>
+        {value?.duration && !isZeroSeconds(parseDuration(value?.duration)) ? (
+          <Time
+            className={clsx('badge', 'badge--info', styles.duration)}
+            dateTime={value.duration}
+            variant="duration"
+          />
+        ) : value?.publishedAt &&
+          isBefore(parseJSON(value.publishedAt), now) ? (
+          <span className={clsx('badge', 'badge--info', styles.liveNow)}>
+            ライブ配信中
+          </span>
+        ) : null}
+      </div>
 
-    <div className={clsx('card__body', styles.content)}>
-      <h4>{value?.title || <Skeleton variant="text" />}</h4>
-    </div>
+      <div className={clsx('card__body', styles.content)}>
+        <h4>{value?.title || <Skeleton variant="text" />}</h4>
+      </div>
 
-    <div className="card__footer">
-      {value?.publishedAt ? (
-        <Time
-          className={styles.published}
-          dateTime={value.publishedAt}
-          variant={timeOptions?.relativeTime ? 'relative' : 'normal'}
-        />
-      ) : (
-        <span className={styles.published}>
-          <Skeleton variant="text" />
-        </span>
-      )}
-    </div>
-  </a>
-)
+      <div className="card__footer">
+        {value?.publishedAt ? (
+          <Time
+            className={styles.published}
+            dateTime={value.publishedAt}
+            variant={timeOptions?.relativeTime ? 'relative' : 'normal'}
+          />
+        ) : (
+          <span className={styles.published}>
+            <Skeleton variant="text" />
+          </span>
+        )}
+      </div>
+    </a>
+  )
+}
 
 export default VideoCard
