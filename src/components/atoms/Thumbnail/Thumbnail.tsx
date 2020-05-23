af77e3794ed78c18054187377cd84c5b8eab2ba9
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-05-23 22:15:33 +0900

    refactor(thumbnail): add skeleton to thumbnail (#790)

--- src/components/atoms/Thumbnail/Thumbnail.tsx
+++ src/components/atoms/Thumbnail/Thumbnail.tsx
@@ -1,26 +1,32 @@
 import React, { FC, memo } from 'react'
 
+import Skeleton from '@/components/atoms/Skeleton'
+
 import styles from './Thumbnail.module.css'
 
 type Props = {
-  id: string
+  id?: string
 }
 
 const Thumbnail: FC<Props> = ({ id }) => (
   <div className={styles.thumbnail}>
-    <img
-      alt=""
-      className={styles.image}
-      height={360}
-      loading="lazy"
-      sizes="(min-width: 500px) 320px, 480px"
-      src={`https://i.ytimg.com/vi/${id}/hqdefault.jpg`}
-      srcSet={[
-        `https://i.ytimg.com/vi/${id}/mqdefault.jpg 320w`,
-        `https://i.ytimg.com/vi/${id}/hqdefault.jpg`
-      ].join(', ')}
-      width={480}
-    />
+    {id ? (
+      <img
+        alt=""
+        className={styles.image}
+        height={360}
+        loading="lazy"
+        sizes="(min-width: 500px) 320px, 480px"
+        src={`https://i.ytimg.com/vi/${id}/hqdefault.jpg`}
+        srcSet={[
+          `https://i.ytimg.com/vi/${id}/mqdefault.jpg 320w`,
+          `https://i.ytimg.com/vi/${id}/hqdefault.jpg`
+        ].join(', ')}
+        width={480}
+      />
+    ) : (
+      <Skeleton className={styles.image} variant="rect" />
+    )}
   </div>
 )
 
