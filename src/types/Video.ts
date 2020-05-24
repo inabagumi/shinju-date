bfd7ba9be5d73378a8613ac272f2d50eb7b432c7
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-05-25 00:28:28 +0900

    perf: introduce imgix (#796)

--- src/types/Video.ts
+++ src/types/Video.ts
@@ -1,12 +1,13 @@
 import type { Duration } from 'date-fns'
 
-import type { Channel } from '@/types'
+import type { Channel, Image } from '@/types'
 
 type Video = {
   channel: Channel
   duration?: Duration
   id: string
   publishedAt: Date
+  thumbnails: Image[]
   title: string
   url: string
 }
--- src/utils/normalize.ts
+++ src/utils/normalize.ts
@@ -1,8 +1,17 @@
 import { ObjectWithObjectID } from '@algolia/client-search'
 import { fromUnixTime } from 'date-fns'
+import ImgixClient from 'imgix-core-js'
 
 import { isZeroSeconds, parseDuration } from '@/utils'
-import type { AlgoliaVideo, Video } from '@/types'
+import type { AlgoliaVideo, Image, Video } from '@/types'
+
+const ASPECT_RATIO = 0.5625
+
+const imgixClient = new ImgixClient({
+  domain: process.env.IMGIX_DOMAIN || 'example.imgix.net',
+  includeLibraryParam: false,
+  secureURLToken: process.env.IMGIX_SECURE_URL_TOKEN
+})
 
 function normalize({
   channel,
@@ -13,12 +22,25 @@ function normalize({
   url
 }: AlgoliaVideo & ObjectWithObjectID): Video {
   const duration = parseDuration(rawDuration ?? 'P0D')
+  const thumbnails = [320, 480].map(
+    (width): Image => ({
+      height: width * ASPECT_RATIO,
+      src: imgixClient.buildURL(`https://i.ytimg.com/vi/${id}/hqdefault.jpg`, {
+        ar: `1:${ASPECT_RATIO}`,
+        auto: 'compress,format',
+        fit: 'crop',
+        width
+      }),
+      width
+    })
+  )
 
   return {
     channel,
     duration: isZeroSeconds(duration) ? undefined : duration,
     id,
     publishedAt: fromUnixTime(publishedAt),
+    thumbnails,
     title,
     url
   }
