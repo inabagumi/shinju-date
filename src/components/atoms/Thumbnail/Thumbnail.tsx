0921e14550397ba0dd2263177d6ed56ac8117c1e
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-05-25 21:44:48 +0900

    perf(thumbnail): use lqip

--- src/components/atoms/Thumbnail/Thumbnail.tsx
+++ src/components/atoms/Thumbnail/Thumbnail.tsx
@@ -9,25 +9,24 @@ type Props = {
   value?: Video
 }
 
-const Thumbnail: FC<Props> = ({ value }) => {
-  return (
-    <div className={styles.thumbnail}>
-      {value ? (
-        <img
-          alt=""
-          className={styles.image}
-          height={value.thumbnail.height}
-          loading="lazy"
-          src={value.thumbnail.src}
-          srcSet={value.thumbnail.srcSet}
-          width={value.thumbnail.width}
-        />
-      ) : (
-        <Skeleton className={styles.image} variant="rect" />
-      )}
-    </div>
-  )
-}
+const Thumbnail: FC<Props> = ({ value }) => (
+  <div className={styles.thumbnail}>
+    {value ? (
+      <img
+        alt=""
+        className={styles.image}
+        height={value.thumbnail.height}
+        loading="lazy"
+        src={value.thumbnail.src}
+        srcSet={value.thumbnail.srcSet}
+        style={{ backgroundImage: `url('${value.thumbnail.preSrc}')` }}
+        width={value.thumbnail.width}
+      />
+    ) : (
+      <Skeleton className={styles.image} variant="rect" />
+    )}
+  </div>
+)
 
 export default memo(
   Thumbnail,
