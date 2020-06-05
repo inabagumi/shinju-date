f1601e5aa166a2e894cb46e8628117960858cfbf
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-06-06 07:56:01 +0900

    feat(header): add icon (#841)

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,6 @@
 export { default as appleTouchIcon } from './apple-touch-icon.png'
 export { default as favicon192x192 } from './favicon-192x192.png'
 export { default as favicon512x512 } from './favicon-512x512.png'
+export { default as Icon } from './icon.svg'
 export { default as Logo } from './logo.svg'
 export { default as mainVisual } from './main-visual.jpg'
