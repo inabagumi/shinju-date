9d9c3e90cd0c9e6195e817932413661fe4edaabb
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-10-02 16:43:42 +0900

    feat(layout): introduce @reach/skip-nav

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,3 +1,4 @@
+import { SkipNavContent, SkipNavLink } from '@reach/skip-nav'
 import type { FC } from 'react'
 
 import Footer from '@/components/Footer'
@@ -5,12 +6,21 @@ import Navbar from '@/components/Navbar'
 
 import styles from './Layout.module.css'
 
+const SKIP_NAV_CONTENT_ID = 'content'
+
 const Layout: FC = ({ children }) => (
   <>
     <div className={styles.wrapper}>
+      <SkipNavLink
+        className={styles.skipNavLink}
+        contentId={SKIP_NAV_CONTENT_ID}
+      >
+        コンテンツにスキップ
+      </SkipNavLink>
+
       <Navbar />
 
-      {children}
+      <SkipNavContent id={SKIP_NAV_CONTENT_ID}>{children}</SkipNavContent>
     </div>
 
     <Footer />
