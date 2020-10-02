870c8dabbdef1e5bd6061f64fdaf8a029c55647e
Yamagishi Kazutoshi <ykzts@haneru.dev>
2020-10-02 16:54:00 +0900

    fix(style): add postcss.config.js (#1059)

---
+++ postcss.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: ['./src/**/*.{mdx,tsx}'],
+        safelist: ['html']
+      }
+    ],
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009'
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false
+        }
+      }
+    ]
+  ]
+}
