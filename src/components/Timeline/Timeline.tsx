b75eef6ecdb0820f114e0fa202d023f9f0feaaf9
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-10-03 01:09:27 +0900

    refactor(timeline): refactor component

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -1,10 +1,13 @@
-import { compareAsc, startOfDay } from 'date-fns'
+import { compareAsc, format, parseJSON, startOfDay } from 'date-fns'
+import jaLocale from 'date-fns/locale/ja'
+import chunk from 'lodash.chunk'
 import groupBy from 'lodash.groupby'
 import { memo, useMemo } from 'react'
 import type { FC } from 'react'
 
+import Skeleton from '@/components/Skeleton'
+import VideoCard from '@/components/VideoCard'
 import type { Video } from '@/types'
-import TimelineSection from './TimelineSection'
 
 const compare = (leftVideo: Video, rightVideo: Video): number =>
   compareAsc(leftVideo.publishedAt, rightVideo.publishedAt)
@@ -27,11 +30,49 @@ const Timeline: FC<Props> = ({ values }) => {
   return (
     <>
       {schedule ? (
-        Object.entries(schedule).map(([day, items]) => (
-          <TimelineSection dateTime={day} items={items} key={day} />
+        Object.entries(schedule).map(([dateTime, items]) => (
+          <section className="margin-top--lg section" key={dateTime}>
+            <h2 className="margin-bottom--lg text--right">
+              <time dateTime={dateTime}>
+                {format(parseJSON(dateTime), 'P', { locale: jaLocale })}
+              </time>
+            </h2>
+
+            <div className="container">
+              {chunk(items, 3).map((values) => (
+                <div
+                  className="row"
+                  key={values.map((value) => value.id).join(':')}
+                >
+                  {values.map((value) => (
+                    <div
+                      className="col col--4 padding-bottom--lg padding-horiz--sm"
+                      key={value.id}
+                    >
+                      <VideoCard value={value} />
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </section>
         ))
       ) : (
-        <TimelineSection />
+        <section className="margin-top--lg section">
+          <h2 className="margin-bottom--lg text--right">
+            <Skeleton variant="text" />
+          </h2>
+          <div className="container">
+            <div className="row">
+              <div className="col col--4 padding-bottom--lg padding-horiz--sm">
+                <VideoCard />
+              </div>
+              <div className="col col--4 padding-bottom--lg padding-horiz--sm">
+                <VideoCard />
+              </div>
+            </div>
+          </div>
+        </section>
       )}
     </>
   )
