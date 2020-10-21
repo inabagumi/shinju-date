e0e951caa94ad53776d9c7ee354215829cd28bb6
Yamagishi Kazutoshi <ykzts@haneru.dev>
2020-10-21 13:27:23 +0900

    ci(eslint): remove duplicate rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,5 @@ module.exports = {
   extends: ['@inabagumi/react', 'plugin:@next/next/recommended'],
   parserOptions: {
     project: 'tsconfig.json'
-  },
-  rules: {
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off'
   }
 }
