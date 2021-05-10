b63187aa65301cbc22061253a7298cdb6b6c6326
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-05-11 00:35:29 +0900

    fix(eslint): add '@next/next/google-font-display': 'off'

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,8 @@ module.exports = {
   extends: ['@inabagumi/react', 'plugin:@next/next/recommended'],
   parserOptions: {
     project: 'tsconfig.json'
+  },
+  rules: {
+    '@next/next/google-font-display': 'off'
   }
 }
