52b64eaa3a80db8e739d11248e85a13a7defdbe2
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-06-23 17:08:21 +0900

    fix(types): add image types

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,12 +1,3 @@
-/// <reference types="@mdx-js/loader" />
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-declare module './*.svg' {
-  import { SVGProps, VFC } from 'react'
-
-  type Props = SVGProps<SVGSVGElement>
-  const content: VFC<Props>
-
-  export default content
-}
+/// <reference types="next/image-types/global" />
