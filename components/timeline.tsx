46d14a316b082bd148aa4d2af7e81e042b74e4f2
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-10-31 00:13:57 +0900

    fix(search): fix skeleton

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -57,21 +57,60 @@ const Timeline: VFC<Props> = ({ values }) => {
           </section>
         ))
       ) : (
-        <section className="margin-top--lg section">
-          <h2 className="margin-bottom--lg text--right">
-            <Skeleton variant="text" />
-          </h2>
-          <div className="container">
-            <div className="row">
-              <div className="col col--4 padding-bottom--lg padding-horiz--sm">
-                <VideoCard />
+        <>
+          <section className="margin-top--lg section">
+            <h2 className="margin-bottom--lg text--right">
+              <Skeleton variant="text" />
+            </h2>
+            <div className="container">
+              <div className="row">
+                <div className="col col--4 padding-bottom--lg padding-horiz--sm">
+                  <VideoCard />
+                </div>
+                <div className="col col--4 padding-bottom--lg padding-horiz--sm">
+                  <VideoCard />
+                </div>
+                <div className="col col--4 padding-bottom--lg padding-horiz--sm">
+                  <VideoCard />
+                </div>
               </div>
-              <div className="col col--4 padding-bottom--lg padding-horiz--sm">
-                <VideoCard />
+              <div className="row">
+                <div className="col col--4 padding-bottom--lg padding-horiz--sm">
+                  <VideoCard />
+                </div>
+                <div className="col col--4 padding-bottom--lg padding-horiz--sm">
+                  <VideoCard />
+                </div>
               </div>
             </div>
-          </div>
-        </section>
+          </section>
+          <section className="margin-top--lg section">
+            <h2 className="margin-bottom--lg text--right">
+              <Skeleton variant="text" />
+            </h2>
+            <div className="container">
+              <div className="row">
+                <div className="col col--4 padding-bottom--lg padding-horiz--sm">
+                  <VideoCard />
+                </div>
+                <div className="col col--4 padding-bottom--lg padding-horiz--sm">
+                  <VideoCard />
+                </div>
+                <div className="col col--4 padding-bottom--lg padding-horiz--sm">
+                  <VideoCard />
+                </div>
+              </div>
+              <div className="row">
+                <div className="col col--4 padding-bottom--lg padding-horiz--sm">
+                  <VideoCard />
+                </div>
+                <div className="col col--4 padding-bottom--lg padding-horiz--sm">
+                  <VideoCard />
+                </div>
+              </div>
+            </div>
+          </section>
+        </>
       )}
     </>
   )
