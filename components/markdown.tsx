f01d8197bf6ef576fe86f1c16af30b52bdd402ca
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-11-01 21:07:05 +0900

    fix(mdx): move mdx provider to page/_app (#1714)

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -1,9 +1,7 @@
-import { MDXProvider, useMDXComponents } from '@mdx-js/react'
 import { useRouter } from 'next/router'
 import { NextSeo } from 'next-seo'
 import Page from './layout'
-import Link, { Props as LinkProps } from './link'
-import type { FunctionComponent } from 'mdx/types'
+import Link from './link'
 import type { ReactNode, VFC } from 'react'
 
 type Props = {
@@ -13,65 +11,58 @@ type Props = {
 
 const Markdown: VFC<Props> = ({ children, title }) => {
   const router = useRouter()
-  const components = useMDXComponents({
-    a: Link as FunctionComponent<LinkProps>
-  })
 
   return (
-    <>
+    <Page>
       {title && <NextSeo title={title} />}
 
-      <Page>
-        {title && (
-          <div className="hero hero--dark">
-            <div className="container">
-              <h1 className="hero__title">{title}</h1>
-            </div>
-          </div>
-        )}
-
-        <div className="container">
-          <nav aria-label="パンくずリスト" className="margin-vert--md">
-            <ul
-              className="breadcrumbs breadcrumbs--sm"
-              itemScope
-              itemType="https://schema.org/BreadcrumbList"
-            >
-              <li
-                className="breadcrumbs__item"
-                itemProp="itemListElement"
-                itemScope
-                itemType="https://schema.org/ListItem"
-              >
-                <Link className="breadcrumbs__link" href="/" itemProp="item">
-                  <span itemProp="name">SHINJU DATE</span>
-                </Link>
-                <meta itemProp="position" content="1" />
-              </li>
-              <li
-                className="breadcrumbs__item breadcrumbs__item--active"
-                itemProp="itemListElement"
-                itemScope
-                itemType="https://schema.org/ListItem"
-              >
-                <Link
-                  className="breadcrumbs__link"
-                  href={router.pathname}
-                  itemProp="item"
-                >
-                  <span itemProp="name">{title || router.pathname}</span>
-                </Link>
-                <meta itemProp="position" content="2" />
-              </li>
-            </ul>
-          </nav>
-
-          <div className="markdown padding-bottom--lg">
-            <MDXProvider components={components}>{children}</MDXProvider>
+      {title && (
+        <div className="hero hero--dark">
+          <div className="container">
+            <h1 className="hero__title">{title}</h1>
           </div>
         </div>
-      </Page>
-    </>
+      )}
+
+      <div className="container">
+        <nav aria-label="パンくずリスト" className="margin-vert--md">
+          <ul
+            className="breadcrumbs breadcrumbs--sm"
+            itemScope
+            itemType="https://schema.org/BreadcrumbList"
+          >
+            <li
+              className="breadcrumbs__item"
+              itemProp="itemListElement"
+              itemScope
+              itemType="https://schema.org/ListItem"
+            >
+              <Link className="breadcrumbs__link" href="/" itemProp="item">
+                <span itemProp="name">SHINJU DATE</span>
+              </Link>
+              <meta itemProp="position" content="1" />
+            </li>
+            <li
+              className="breadcrumbs__item breadcrumbs__item--active"
+              itemProp="itemListElement"
+              itemScope
+              itemType="https://schema.org/ListItem"
+            >
+              <Link
+                className="breadcrumbs__link"
+                href={router.pathname}
+                itemProp="item"
+              >
+                <span itemProp="name">{title || router.pathname}</span>
+              </Link>
+              <meta itemProp="position" content="2" />
+            </li>
+          </ul>
+        </nav>
+
+        <div className="markdown padding-bottom--lg">{children}</div>
+      </div>
+    </Page>
   )
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/global.css'
+import { MDXProvider } from '@mdx-js/react'
 import Head from 'next/head'
 import Script from 'next/script'
 import { DefaultSeo } from 'next-seo'
@@ -10,9 +11,15 @@ import appleTouchIcon from '../assets/apple-touch-icon.png'
 import favicon192x192 from '../assets/favicon-192x192.png'
 import favicon512x512 from '../assets/favicon-512x512.png'
 import ProgressBar from '../components/progress-bar'
+import Link, { Props as LinkProps } from '../components/link'
 import fetcher from '../utils/fetcher'
+import type { FunctionComponent, MDXComponents } from 'mdx/types'
 import type { AppProps } from 'next/app'
 import type { VFC } from 'react'
+
+const mdxComponents: MDXComponents = {
+  a: Link as FunctionComponent<LinkProps>
+}
 
 const MyApp: VFC<AppProps> = ({ Component, pageProps, router }) => {
   const handleRouterChangeComplete = useCallback((url: string) => {
@@ -41,54 +48,56 @@ const MyApp: VFC<AppProps> = ({ Component, pageProps, router }) => {
     >
       <SWRConfig value={{ fetcher }}>
         <ThemeProvider defaultTheme="system">
-          <Head>
-            <link
-              href={favicon192x192.src}
-              rel="icon"
-              sizes={`${favicon192x192.width}x${favicon192x192.height}`}
-              type="image/png"
-            />
-            <link
-              href={favicon512x512.src}
-              rel="icon"
-              sizes={`${favicon512x512.width}x${favicon512x512.height}`}
-              type="image/png"
-            />
-            <link
-              href={appleTouchIcon.src}
-              rel="apple-touch-icon"
-              sizes={`${appleTouchIcon.width}x${appleTouchIcon.height}`}
-              type="image/png"
-            />
-          </Head>
-
-          {process.env.NEXT_PUBLIC_GA_TRACKING_ID && (
-            <>
-              <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
-                strategy="afterInteractive"
+          <MDXProvider components={mdxComponents}>
+            <Head>
+              <link
+                href={favicon192x192.src}
+                rel="icon"
+                sizes={`${favicon192x192.width}x${favicon192x192.height}`}
+                type="image/png"
               />
-              <Script
-                id="gtag-init"
-                dangerouslySetInnerHTML={{
-                  __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-
-                    gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
-                      page_path: window.location.pathname
-                    });
-                  `
-                }}
-                strategy="afterInteractive"
+              <link
+                href={favicon512x512.src}
+                rel="icon"
+                sizes={`${favicon512x512.width}x${favicon512x512.height}`}
+                type="image/png"
               />
-            </>
-          )}
+              <link
+                href={appleTouchIcon.src}
+                rel="apple-touch-icon"
+                sizes={`${appleTouchIcon.width}x${appleTouchIcon.height}`}
+                type="image/png"
+              />
+            </Head>
 
-          <DefaultSeo titleTemplate="%s - SHINJU DATE" />
-          <ProgressBar options={{ showSpinner: false }} />
-          <Component {...pageProps} />
+            {process.env.NEXT_PUBLIC_GA_TRACKING_ID && (
+              <>
+                <Script
+                  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+                  strategy="afterInteractive"
+                />
+                <Script
+                  id="gtag-init"
+                  dangerouslySetInnerHTML={{
+                    __html: `
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+
+                      gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+                        page_path: window.location.pathname
+                      });
+                    `
+                  }}
+                  strategy="afterInteractive"
+                />
+              </>
+            )}
+
+            <DefaultSeo titleTemplate="%s - SHINJU DATE" />
+            <ProgressBar options={{ showSpinner: false }} />
+            <Component {...pageProps} />
+          </MDXProvider>
         </ThemeProvider>
       </SWRConfig>
     </IntlProvider>
