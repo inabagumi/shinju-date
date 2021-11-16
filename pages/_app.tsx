6ddf42f2984147e0fa02b6ed96c943914f0580d4
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-11-16 23:49:40 +0900

    refactor: move mdxprovider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import '../styles/global.css'
-import { MDXProvider } from '@mdx-js/react'
 import { DefaultSeo } from 'next-seo'
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
@@ -10,15 +9,9 @@ import { SWRConfig } from 'swr'
 import appleTouchIcon from '../assets/apple-touch-icon.png'
 import favicon192x192 from '../assets/favicon-192x192.png'
 import favicon512x512 from '../assets/favicon-512x512.png'
-import Link, { Props as LinkProps } from '../components/link'
 import ProgressBar from '../components/progress-bar'
-import type { FunctionComponent, MDXComponents } from 'mdx/types'
 import type { AppProps } from 'next/app'
 import type { VFC } from 'react'
-
-const mdxComponents: MDXComponents = {
-  a: Link as FunctionComponent<LinkProps>
-}
 
 const MyApp: VFC<AppProps> = ({ Component, pageProps, router }) => {
   const handleRouterChangeComplete = useCallback((url: string) => {
@@ -47,75 +40,73 @@ const MyApp: VFC<AppProps> = ({ Component, pageProps, router }) => {
     >
       <SWRConfig>
         <ThemeProvider defaultTheme="system">
-          <MDXProvider components={mdxComponents}>
-            <Head>
-              <link
-                href={favicon192x192.src}
-                rel="icon"
-                sizes={`${favicon192x192.width}x${favicon192x192.height}`}
-                type="image/png"
-              />
-              <link
-                href={favicon512x512.src}
-                rel="icon"
-                sizes={`${favicon512x512.width}x${favicon512x512.height}`}
-                type="image/png"
-              />
-              <link
-                href={appleTouchIcon.src}
-                rel="apple-touch-icon"
-                sizes={`${appleTouchIcon.width}x${appleTouchIcon.height}`}
-                type="image/png"
-              />
-            </Head>
-
-            {process.env.NEXT_PUBLIC_GA_TRACKING_ID && (
-              <>
-                <Script
-                  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
-                  strategy="afterInteractive"
-                />
-                <Script
-                  dangerouslySetInnerHTML={{
-                    __html: `
-                      window.dataLayer = window.dataLayer || [];
-                      function gtag(){dataLayer.push(arguments);}
-                      gtag('js', new Date());
-
-                      gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
-                        page_path: window.location.pathname
-                      });
-                    `
-                  }}
-                  id="gtag-init"
-                  strategy="afterInteractive"
-                />
-              </>
-            )}
-
-            <DefaultSeo
-              description={process.env.NEXT_PUBLIC_DESCRIPTION}
-              openGraph={{
-                images: [
-                  {
-                    height: 630,
-                    url: new URL(
-                      new URL('../assets/share-card.jpg', import.meta.url),
-                      process.env.NEXT_PUBLIC_BASE_URL
-                    ).toString(),
-                    width: 1200
-                  }
-                ],
-                type: 'website'
-              }}
-              titleTemplate="%s - SHINJU DATE"
-              twitter={{
-                cardType: 'summary_large_image'
-              }}
+          <Head>
+            <link
+              href={favicon192x192.src}
+              rel="icon"
+              sizes={`${favicon192x192.width}x${favicon192x192.height}`}
+              type="image/png"
             />
-            <ProgressBar options={{ showSpinner: false }} />
-            <Component {...pageProps} />
-          </MDXProvider>
+            <link
+              href={favicon512x512.src}
+              rel="icon"
+              sizes={`${favicon512x512.width}x${favicon512x512.height}`}
+              type="image/png"
+            />
+            <link
+              href={appleTouchIcon.src}
+              rel="apple-touch-icon"
+              sizes={`${appleTouchIcon.width}x${appleTouchIcon.height}`}
+              type="image/png"
+            />
+          </Head>
+
+          {process.env.NEXT_PUBLIC_GA_TRACKING_ID && (
+            <>
+              <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+                strategy="afterInteractive"
+              />
+              <Script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+                      page_path: window.location.pathname
+                    });
+                  `
+                }}
+                id="gtag-init"
+                strategy="afterInteractive"
+              />
+            </>
+          )}
+
+          <DefaultSeo
+            description={process.env.NEXT_PUBLIC_DESCRIPTION}
+            openGraph={{
+              images: [
+                {
+                  height: 630,
+                  url: new URL(
+                    new URL('../assets/share-card.jpg', import.meta.url),
+                    process.env.NEXT_PUBLIC_BASE_URL
+                  ).toString(),
+                  width: 1200
+                }
+              ],
+              type: 'website'
+            }}
+            titleTemplate="%s - SHINJU DATE"
+            twitter={{
+              cardType: 'summary_large_image'
+            }}
+          />
+          <ProgressBar options={{ showSpinner: false }} />
+          <Component {...pageProps} />
         </ThemeProvider>
       </SWRConfig>
     </IntlProvider>
