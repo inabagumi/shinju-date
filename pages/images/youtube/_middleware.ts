af1b15624262bdc9368bfcbbbb69045121fbf24c
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-11-18 09:05:00 +0900

    refactor: use basename (#1767)

--- pages/images/youtube/_middleware.ts
+++ pages/images/youtube/_middleware.ts
@@ -1,36 +1,14 @@
 import { NextResponse } from 'next/server'
-import { match } from 'path-to-regexp'
+import { basename } from 'path'
 import type { NextRequest } from 'next/server'
-
-type Params = {
-  id: string
-  locale?: string
-}
-
-const urlMatches = [
-  match<Params>('/images/youtube/:id.jpg'),
-  match<Params>('/:locale/images/youtube/:id.jpg')
-]
 
 export async function middleware(
   req: NextRequest
 ): Promise<NextResponse | Response | undefined> {
-  let id: string | undefined
+  const id = basename(req.nextUrl.pathname, '.jpg')
+  const res = await fetch(`https://i.ytimg.com/vi/${id}/maxresdefault.jpg`)
 
-  for (const urlMatch of urlMatches) {
-    const m = urlMatch(req.nextUrl.pathname)
-    if (m) {
-      id = m.params.id
-    }
-  }
-
-  if (id) {
-    const res = await fetch(`https://i.ytimg.com/vi/${id}/maxresdefault.jpg`)
-
-    return res.ok
-      ? res
-      : NextResponse.rewrite(`https://i.ytimg.com/vi/${id}/hqdefault.jpg`)
-  }
-
-  return
+  return res.ok
+    ? res
+    : NextResponse.rewrite(`https://i.ytimg.com/vi/${id}/hqdefault.jpg`)
 }
