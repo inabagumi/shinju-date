370d000d4fa0428491989ba80c5bb48a00dc1730
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-11-22 20:13:51 +0900

    fix(document): replace to class based component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,30 +1,29 @@
-import { Head, Html, Main, NextScript } from 'next/document'
-import type { DocumentProps } from 'next/document'
-import type { VFC } from 'react'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 
-const Document: VFC<DocumentProps> = () => {
-  return (
-    <Html>
-      <Head>
-        <meta content="#212121" name="theme-color" />
-        <link href="/manifest.json" rel="manifest" />
-        <link
-          href="/opensearch.xml"
-          rel="search"
-          type="application/opensearchdescription+xml"
-        />
-        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
-        <link
-          href="https://fonts.googleapis.com/css2?display=swap&amp;family=Lato%3Awght%40300%3B400%3B700"
-          rel="stylesheet"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta content="#212121" name="theme-color" />
+          <link href="/manifest.json" rel="manifest" />
+          <link
+            href="/opensearch.xml"
+            rel="search"
+            type="application/opensearchdescription+xml"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?display=swap&amp;family=Lato%3Awght%40300%3B400%3B700"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
 
-export default Document
+export default MyDocument
