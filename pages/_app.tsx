3ab845fd0c275c82c2fbbec173bd988075f15189
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-12-06 02:50:44 +0900

    fix(videos): remove unnessary index.ts (#1856)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,6 @@ import Script from 'next/script'
 import { type VFC, useCallback, useEffect } from 'react'
 import { IntlProvider } from 'react-intl'
 import aa from 'search-insights'
-import { SWRConfig } from 'swr'
 import appleTouchIcon from '../assets/apple-touch-icon.png'
 import favicon192x192 from '../assets/favicon-192x192.png'
 import favicon512x512 from '../assets/favicon-512x512.png'
@@ -44,84 +43,78 @@ const MyApp: VFC<AppProps> = ({ Component, pageProps, router }) => {
   }, [router.events, handleRouterChangeComplete])
 
   return (
-    <IntlProvider
-      defaultLocale={router.defaultLocale}
-      locale={router.locale ?? 'en'}
-      timeZone="Asia/Tokyo"
-    >
-      <SWRConfig>
-        <ThemeProvider defaultTheme="system">
-          <GroupProvider>
-            <Head>
-              <link
-                href={favicon192x192.src}
-                rel="icon"
-                sizes={`${favicon192x192.width}x${favicon192x192.height}`}
-                type="image/png"
-              />
-              <link
-                href={favicon512x512.src}
-                rel="icon"
-                sizes={`${favicon512x512.width}x${favicon512x512.height}`}
-                type="image/png"
-              />
-              <link
-                href={appleTouchIcon.src}
-                rel="apple-touch-icon"
-                sizes={`${appleTouchIcon.width}x${appleTouchIcon.height}`}
-                type="image/png"
-              />
-            </Head>
-
-            {process.env.NEXT_PUBLIC_GA_TRACKING_ID && (
-              <>
-                <Script
-                  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
-                  strategy="afterInteractive"
-                />
-                <Script
-                  dangerouslySetInnerHTML={{
-                    __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-
-                    gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
-                      page_path: window.location.pathname
-                    });
-                  `
-                  }}
-                  id="gtag-init"
-                  strategy="afterInteractive"
-                />
-              </>
-            )}
-
-            <DefaultSeo
-              description={process.env.NEXT_PUBLIC_DESCRIPTION}
-              openGraph={{
-                images: [
-                  {
-                    height: shareCard.height,
-                    url: new URL(
-                      shareCard.src,
-                      process.env.NEXT_PUBLIC_BASE_URL
-                    ).toString(),
-                    width: shareCard.width
-                  }
-                ],
-                type: 'website'
-              }}
-              titleTemplate="%s - SHINJU DATE"
-              twitter={{
-                cardType: 'summary_large_image'
-              }}
+    <IntlProvider locale="ja" timeZone="Asia/Tokyo">
+      <ThemeProvider defaultTheme="system">
+        <GroupProvider>
+          <Head>
+            <link
+              href={favicon192x192.src}
+              rel="icon"
+              sizes={`${favicon192x192.width}x${favicon192x192.height}`}
+              type="image/png"
             />
-            <ProgressBar options={{ showSpinner: false }} />
-            <Component {...pageProps} />
-          </GroupProvider>
-        </ThemeProvider>
-      </SWRConfig>
+            <link
+              href={favicon512x512.src}
+              rel="icon"
+              sizes={`${favicon512x512.width}x${favicon512x512.height}`}
+              type="image/png"
+            />
+            <link
+              href={appleTouchIcon.src}
+              rel="apple-touch-icon"
+              sizes={`${appleTouchIcon.width}x${appleTouchIcon.height}`}
+              type="image/png"
+            />
+          </Head>
+
+          {process.env.NEXT_PUBLIC_GA_TRACKING_ID && (
+            <>
+              <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+                strategy="afterInteractive"
+              />
+              <Script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+
+                  gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+                    page_path: window.location.pathname
+                  });
+                `
+                }}
+                id="gtag-init"
+                strategy="afterInteractive"
+              />
+            </>
+          )}
+
+          <DefaultSeo
+            description={process.env.NEXT_PUBLIC_DESCRIPTION}
+            openGraph={{
+              images: [
+                {
+                  height: shareCard.height,
+                  url: new URL(
+                    shareCard.src,
+                    process.env.NEXT_PUBLIC_BASE_URL
+                  ).toString(),
+                  width: shareCard.width
+                }
+              ],
+              type: 'website'
+            }}
+            titleTemplate="%s - SHINJU DATE"
+            twitter={{
+              cardType: 'summary_large_image'
+            }}
+          />
+          <ProgressBar options={{ showSpinner: false }} />
+          <Component {...pageProps} />
+        </GroupProvider>
+      </ThemeProvider>
     </IntlProvider>
   )
 }
