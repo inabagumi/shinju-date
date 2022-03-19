eaf9b4146399fcf4b914ef6c12caeae0506ae2ef
Yamagishi Kazutoshi <ykzts@haneru.dev>
2022-03-19 20:55:56 +0900

    ci(terraform): introduce terraform (#2003)

---
+++ terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "algolia_api_key" {}
+variable "algolia_application_id" {}
+variable "supabase_anon_key" {}
+variable "supabase_url" {}
+variable "vercel_api_token" {}
+variable "vercel_team_id" {}
