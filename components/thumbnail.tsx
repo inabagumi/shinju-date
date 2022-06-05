f3a62cb267a6039fe9da75b516a5996169016637
Yamagishi Kazutoshi <ykzts@haneru.dev>
2022-06-05 12:58:47 +0900

    feat: good-bye imgix (#2134)

--- components/thumbnail.tsx
+++ components/thumbnail.tsx
@@ -4,7 +4,6 @@ import { type Image as ImageObject } from '../lib/algolia'
 import Skeleton from './skeleton'
 import styles from './thumbnail.module.css'
 
-const thumbnailBasePath = '/images/youtube'
 const defaultPreSrc =
   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVQI12NgYAAAAAMAASDVlMcAAAAASUVORK5CYII='
 
@@ -16,13 +15,9 @@ type Props = {
 const Thumbnail: FC<Props> = ({ alt = '', value }) => {
   const width = value?.width ?? 1920
   const height = width * 0.5625
-  const src = value?.src.replace(
-    /^https:\/\/i\.ytimg\.com\/vi\/([^/]+)\/[^.]+\.jpg$/,
-    (_, id: string) => `${thumbnailBasePath}/${id}.jpg`
-  )
   const preSrc = value?.preSrc ?? defaultPreSrc
 
-  return src ? (
+  return value?.src ? (
     <Image
       alt={alt}
       blurDataURL={preSrc}
@@ -32,7 +27,7 @@ const Thumbnail: FC<Props> = ({ alt = '', value }) => {
       objectPosition="center"
       placeholder="blur"
       sizes="(max-width: 996px) 100vw, 30vw"
-      src={src}
+      src={value.src}
       width={width}
     />
   ) : (
--- middleware.ts
+++ middleware.ts
@@ -1,47 +1,7 @@
 // eslint-disable-next-line @next/next/no-server-import-in-page
 import { type NextMiddleware, NextResponse } from 'next/server'
-import { isImageResponse, isRequestFromImgix } from './lib/imgix'
 
-const THUMBNAIL_PATH_REGEXP =
-  /^\/images\/youtube\/(?<id>[^./]+?)\.(?:jpg|webp)$/i
-
-export const middleware: NextMiddleware = async (req) => {
-  const remoteImageMatch = req.nextUrl.pathname.match(THUMBNAIL_PATH_REGEXP)
-  const youtubeVideoID = remoteImageMatch?.groups?.id
-
-  if (youtubeVideoID) {
-    const res = await fetch(
-      `https://i.ytimg.com/vi/${youtubeVideoID}/maxresdefault.jpg`,
-      {
-        method: 'HEAD'
-      }
-    )
-
-    return res.ok
-      ? NextResponse.rewrite(res.url)
-      : NextResponse.rewrite(
-          `https://i.ytimg.com/vi/${youtubeVideoID}/hqdefault.jpg`
-        )
-  }
-
-  if (
-    isRequestFromImgix({
-      ip: req.ip ?? '127.0.0.1',
-      ua: req.ua?.ua ?? ''
-    })
-  ) {
-    const res = await fetch(req, {
-      method: 'HEAD'
-    })
-
-    return isImageResponse(res)
-      ? NextResponse.next()
-      : new NextResponse(null, {
-          status: 404,
-          statusText: 'Not Found'
-        })
-  }
-
+export const middleware: NextMiddleware = (req) => {
   if (req.nextUrl.pathname.endsWith('/videos')) {
     const query = req.nextUrl.searchParams.getAll('q').filter(Boolean).join(' ')
 
