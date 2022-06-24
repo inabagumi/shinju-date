a7c5ff3069261c8b7e7c81dc0ce3bde53e6ea95c
Yamagishi Kazutoshi <ykzts@haneru.dev>
2022-06-24 09:54:02 +0000

    feat: introduce fetch requester

---
+++ lib/algolia/index.ts
@@ -0,0 +1,37 @@
+import { createNullCache } from '@algolia/cache-common'
+import algoliasearch, {
+  type SearchClient,
+  type SearchIndex
+} from 'algoliasearch/lite'
+import { createFetchRequester } from './requesters'
+
+export type SearchOptions = {
+  filters?: string[]
+  limit?: number
+  page?: number
+  query?: string
+}
+
+let client: SearchClient
+
+export function getClient(): SearchClient {
+  client ??= algoliasearch(
+    process.env.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID,
+    process.env.NEXT_PUBLIC_ALGOLIA_API_KEY,
+    {
+      requester: createFetchRequester(),
+      requestsCache: createNullCache(),
+      responsesCache: createNullCache()
+    }
+  )
+
+  return client
+}
+
+export function getDefaultIndex(): SearchIndex {
+  const client = getClient()
+
+  return client.initIndex(process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME)
+}
+
+export * from './videos'
--- lib/algolia/videos.ts
+++ lib/algolia/videos.ts
@@ -1,15 +1,4 @@
-import { createNullCache } from '@algolia/cache-common'
-import algoliasearch, {
-  type SearchClient,
-  type SearchIndex
-} from 'algoliasearch/lite'
-
-export type SearchOptions = {
-  filters?: string[]
-  limit?: number
-  page?: number
-  query?: string
-}
+import { type SearchOptions, getDefaultIndex } from '.'
 
 export type Image = {
   height: number
@@ -44,27 +33,6 @@ export async function getChannelByID(id: string) {
   }
 
   return videos[0].channel
-}
-
-let client: SearchClient
-
-export function getClient(): SearchClient {
-  client ??= algoliasearch(
-    process.env.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID,
-    process.env.NEXT_PUBLIC_ALGOLIA_API_KEY,
-    {
-      requestsCache: createNullCache(),
-      responsesCache: createNullCache()
-    }
-  )
-
-  return client
-}
-
-export function getDefaultIndex(): SearchIndex {
-  const client = getClient()
-
-  return client.initIndex(process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME)
 }
 
 export function getVideosByChannelIDs(
