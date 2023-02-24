15c046a2eb6b98ce29ec005011233afd4d1f258f
Yamagishi Kazutoshi <ykzts@haneru.dev>
2023-02-24 11:03:25 +0000

    refactor: replace to `edge-sitemap`

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,5 +1,5 @@
+import { type SitemapItem, SitemapStream } from 'edge-sitemap'
 import { NextResponse } from 'next/server'
-import { type SitemapItem, SitemapStream } from '@/lib/sitemap'
 import { getAllChannels, getAllGroups } from '@/lib/supabase'
 
 const DEFINED_PAGES: SitemapItem[] = [
@@ -32,52 +32,61 @@ const DEFINED_PAGES: SitemapItem[] = [
 
 export const revalidate = 300
 
-export function GET() {
+async function writeSitemap(
+  writableStream: WritableStream<SitemapItem>
+): Promise<void> {
+  const writer = writableStream.getWriter()
+
+  await writer.ready
+
+  try {
+    await Promise.all([
+      ...DEFINED_PAGES.map((sitemapItem) => writer.write(sitemapItem)),
+      getAllChannels().then((channels) =>
+        Promise.all(
+          channels.flatMap((channel) => [
+            writer.write({
+              changefreq: 'daily',
+              loc: `/channels/${channel.slug}`,
+              priority: 0.7
+            }),
+            writer.write({
+              changefreq: 'daily',
+              loc: `/channels/${channel.slug}/videos`,
+              priority: 0.7
+            })
+          ])
+        )
+      ),
+      getAllGroups().then((groups) =>
+        Promise.all(
+          groups.flatMap((group) => [
+            writer.write({
+              changefreq: 'daily',
+              loc: `/groups/${group.slug}`,
+              priority: 0.7
+            }),
+            writer.write({
+              changefreq: 'daily',
+              loc: `/groups/${group.slug}/videos`,
+              priority: 0.7
+            })
+          ])
+        )
+      )
+    ])
+  } finally {
+    await writer.close()
+  }
+}
+
+export function GET(): NextResponse {
   const { readable: smReadable, writable: smWritable } = new SitemapStream({
     baseURL: new URL(process.env.NEXT_PUBLIC_BASE_URL)
   })
   const body = smReadable.pipeThrough(new TextEncoderStream())
 
-  const writer = smWritable.getWriter()
-
-  void writer.ready
-    .then(() => Promise.all([getAllChannels(), getAllGroups()]))
-    .then(([channels, groups]) =>
-      Promise.allSettled([
-        ...DEFINED_PAGES.map((sitemapItem) => writer.write(sitemapItem)),
-        ...channels.map((channel) =>
-          writer.write({
-            changefreq: 'daily',
-            loc: `/channels/${channel.slug}`,
-            priority: 0.7
-          })
-        ),
-        ...channels.map((channel) =>
-          writer.write({
-            changefreq: 'daily',
-            loc: `/channels/${channel.slug}/videos`,
-            priority: 0.7
-          })
-        ),
-        ...groups.map((group) =>
-          writer.write({
-            changefreq: 'daily',
-            loc: `/groups/${group.slug}`,
-            priority: 0.7
-          })
-        ),
-        ...groups.map((group) =>
-          writer.write({
-            changefreq: 'daily',
-            loc: `/groups/${group.slug}/videos`,
-            priority: 0.7
-          })
-        )
-      ])
-    )
-    .finally(() => {
-      void writer.close()
-    })
+  void writeSitemap(smWritable)
 
   return new NextResponse(body, {
     headers: {
