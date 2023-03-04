2ee14b224eb40979db4518a012edc99f93d7d484
Yamagishi Kazutoshi <ykzts@haneru.dev>
2023-03-04 12:19:38 +0900

    refactor(web): `Link` component to `mdx-components.tsx` (#2651)

--- apps/web/mdx-components.tsx
+++ apps/web/mdx-components.tsx
@@ -1,6 +1,24 @@
-import { type FunctionComponent, type MDXComponents } from 'mdx/types'
-import Link, { Props as LinkProps } from '@/ui/link'
+import { type MDXComponents } from 'mdx/types'
+import NextLink from 'next/link'
+import { type ComponentPropsWithoutRef } from 'react'
+
+function Link({
+  href,
+  rel: originalRel,
+  target: originalTarget,
+  ...props
+}: ComponentPropsWithoutRef<'a'>): JSX.Element {
+  if (href && href.startsWith('/')) {
+    return <NextLink href={href} {...props} />
+  }
+
+  const isExternal = !!href && /^https?:\/\//.test(href)
+  const rel = originalRel ?? isExternal ? 'noopener noreferrer' : undefined
+  const target = originalTarget ?? isExternal ? '_blank' : undefined
+
+  return <a href={href} rel={rel} target={target} {...props} />
+}
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return { a: Link as FunctionComponent<LinkProps>, ...components }
+  return { a: Link, ...components }
 }
