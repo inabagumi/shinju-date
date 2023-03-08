124530cd8a5425b5d0e3709aa8d1d3a21ea50681
Yamagishi Kazutoshi <ykzts@haneru.dev>
2023-03-08 16:05:10 +0900

    feat(dns): enable custom domain

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,12 +1,3 @@
-resource "vercel_dns_record" "google_site_verification" {
-  domain  = "shinju.date"
-  name    = ""
-  team_id = vercel_project.this.team_id
-  ttl     = 60
-  type    = "TXT"
-  value   = "google-site-verification=B-AnwaokGJSxLYWtDEFi_NDp-GxH-aLGAMT8SsxTFL4"
-}
-
 resource "vercel_dns_record" "digicert_caa" {
   domain  = "shinju.date"
   name    = "api"
@@ -16,29 +7,20 @@ resource "vercel_dns_record" "digicert_caa" {
   value   = "0 issue \"digicert.com\""
 }
 
-resource "vercel_dns_record" "supabase_verification2" {
+resource "vercel_dns_record" "google_site_verification" {
+  domain  = "shinju.date"
+  name    = ""
+  team_id = vercel_project.this.team_id
+  ttl     = 60
+  type    = "TXT"
+  value   = "google-site-verification=B-AnwaokGJSxLYWtDEFi_NDp-GxH-aLGAMT8SsxTFL4"
+}
+
+resource "vercel_dns_record" "api" {
   domain  = "shinju.date"
   name    = "api"
   team_id = vercel_project.this.team_id
   ttl     = 60
-  type    = "TXT"
-  value   = "ca3-4352c6a9ace743d1aee610af343f402d"
-}
-
-# resource "vercel_dns_record" "api" {
-#   domain  = "shinju.date"
-#   name    = "api"
-#   team_id = vercel_project.this.team_id
-#   ttl     = 60
-#   type    = "CNAME"
-#   value   = "bxbalbupmqhcdqrmoiwl.supabase.co."
-# }
-
-resource "vercel_dns_record" "supabase_verification" {
-  domain  = "shinju.date"
-  name    = "_cf-custom-hostname.api"
-  team_id = vercel_project.this.team_id
-  ttl     = 60
-  type    = "TXT"
-  value   = "62231763-ae47-41bd-aaa1-4c4ad223424d"
+  type    = "CNAME"
+  value   = "bxbalbupmqhcdqrmoiwl.supabase.co."
 }
