507daa8983fc9d5daac5ce367f18d1033805ce8f
Yamagishi Kazutoshi <ykzts@haneru.dev>
2023-03-08 17:13:56 +0900

    fix(admin): disable environment (#2691)

--- apps/admin/app/provider.tsx
+++ apps/admin/app/provider.tsx
@@ -14,7 +14,7 @@ type Props = {
 export default function Provider({ children, session }: Props) {
   return (
     <CacheProvider>
-      <ChakraProvider theme={theme}>
+      <ChakraProvider disableEnvironment theme={theme}>
         <SessionProvider session={session}>{children}</SessionProvider>
       </ChakraProvider>
     </CacheProvider>
