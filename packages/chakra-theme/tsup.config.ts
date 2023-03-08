351441d395e3126c6bed913e846963ed803cd9a5
Yamagishi Kazutoshi <ykzts@haneru.dev>
2023-03-08 13:50:14 +0000

    ci(lint): add lint step

--- packages/chakra-theme/tsup.config.ts
+++ packages/chakra-theme/tsup.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   dts: true,
   entry: ['./src/index.ts'],
+  format: ['cjs', 'esm'],
   onSuccess:
     'chakra-cli tokens ./dist/index.cjs --out ./dist/theming.types.d.ts --strict-component-types --strict-token-types --template augmentation',
-  format: ['cjs', 'esm'],
   target: 'es2022'
 })
