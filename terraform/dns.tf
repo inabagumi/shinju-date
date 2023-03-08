39f1d39cc24eea7206bf9f45788a3200cc0abb33
Yamagishi Kazutoshi <ykzts@haneru.dev>
2023-03-08 15:46:06 +0900

    fix(dns): add caa record (#2686)

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -7,13 +7,13 @@ resource "vercel_dns_record" "google_site_verification" {
   value   = "google-site-verification=B-AnwaokGJSxLYWtDEFi_NDp-GxH-aLGAMT8SsxTFL4"
 }
 
-resource "vercel_dns_record" "supabase_verification" {
+resource "vercel_dns_record" "digicert_caa" {
   domain  = "shinju.date"
-  name    = "_cf-custom-hostname.api"
+  name    = "api"
   team_id = vercel_project.this.team_id
   ttl     = 60
-  type    = "TXT"
-  value   = "62231763-ae47-41bd-aaa1-4c4ad223424d"
+  type    = "CAA"
+  value   = "0 issue \"digicert.com\""
 }
 
 resource "vercel_dns_record" "supabase_verification2" {
@@ -33,3 +33,12 @@ resource "vercel_dns_record" "supabase_verification2" {
 #   type    = "CNAME"
 #   value   = "bxbalbupmqhcdqrmoiwl.supabase.co."
 # }
+
+resource "vercel_dns_record" "supabase_verification" {
+  domain  = "shinju.date"
+  name    = "_cf-custom-hostname.api"
+  team_id = vercel_project.this.team_id
+  ttl     = 60
+  type    = "TXT"
+  value   = "62231763-ae47-41bd-aaa1-4c4ad223424d"
+}
