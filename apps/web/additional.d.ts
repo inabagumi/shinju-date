207092595c67703d8938cab2b0dc437a9e37bd3e
Yamagishi Kazutoshi <ykzts@haneru.dev>
2023-03-09 21:49:24 +0900

    feat(admin): improve login form (#2702)

--- apps/web/additional.d.ts
+++ apps/web/additional.d.ts
@@ -8,7 +8,6 @@ declare namespace NodeJS {
     readonly NEXT_PUBLIC_ALGOLIA_API_KEY: string
     readonly NEXT_PUBLIC_ALGOLIA_INDEX_NAME: string
     readonly NEXT_PUBLIC_BASE_URL: string
-    readonly NEXT_PUBLIC_DESCRIPTION: string
     readonly NEXT_PUBLIC_GA_TRACKING_ID?: string
     readonly NEXT_PUBLIC_SUPABASE_ANON_KEY: string
     readonly NEXT_PUBLIC_SUPABASE_URL: string
