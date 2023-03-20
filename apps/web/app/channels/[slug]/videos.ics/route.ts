6d376583f845bfa779f8b1b64290692475d894e4
Yamagishi Kazutoshi <ykzts@haneru.dev>
2023-03-21 04:14:33 +0900

    feat(web): replace to supabase (#2775)

--- apps/web/app/channels/[slug]/videos.ics/route.ts
+++ apps/web/app/channels/[slug]/videos.ics/route.ts
@@ -1,12 +1,11 @@
 import { Temporal } from '@js-temporal/polyfill'
 import { type NextRequest, NextResponse } from 'next/server'
-import { getVideosByChannelIDs } from '@/lib/algolia'
 import {
   createCalendarResponse,
   createEventAttributesList,
   createNotFoundResponse
 } from '@/lib/calendar'
-import { getChannelBySlug } from '@/lib/supabase'
+import { supabase } from '@/lib/supabase'
 
 export const runtime = 'edge'
 export const dynamic = 'force-static'
@@ -24,18 +23,48 @@ export async function GET(
   _req: NextRequest,
   { params }: Props
 ): Promise<NextResponse> {
-  const channel = await getChannelBySlug(params.slug)
+  const { count, error } = await supabase
+    .from('channels')
+    .select('*', { count: 'exact', head: true })
+    .eq('slug', params.slug)
+    .is('deleted_at', null)
 
-  if (!channel) {
+  if (error) {
+    return new NextResponse(error.message, {
+      headers: {
+        'Content-Type': 'text/plain; charset=UTF-8'
+      },
+      status: 500
+    })
+  }
+
+  if (!count || count < 1) {
     return createNotFoundResponse()
   }
 
   const timeZone = Temporal.TimeZone.from('UTC')
   const now = Temporal.Now.zonedDateTimeISO(timeZone)
-  const videos = await getVideosByChannelIDs([channel.slug], {
-    filters: [`publishedAt < ${now.add({ days: 7 }).epochSeconds}`],
-    limit: 100
-  })
+  const { data: videos, error: secondError } = await supabase
+    .from('videos')
+    .select(
+      'channels!inner (name, slug), duration, published_at, slug, title, url'
+    )
+    .is('deleted_at', null)
+    .is('channels.deleted_at', null)
+    .eq('channels.slug', params.slug)
+    .lt('published_at', now.add({ days: 7 }).toInstant().toJSON())
+    .order('published_at', { ascending: false })
+    .limit(100)
+
+  if (secondError) {
+    return new NextResponse(secondError.message, {
+      headers: {
+        'Content-Type': 'text/plain; charset=UTF-8'
+      },
+      status: 500
+    })
+  }
+
   const events = createEventAttributesList(videos, { now })
 
   return createCalendarResponse(events)
