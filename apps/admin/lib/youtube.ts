95e6b3a715bd33e415d71778aef42291ca97d625
Yamagishi Kazutoshi <ykzts@haneru.dev>
2023-04-01 01:28:41 +0900

    fix(admin): disable native fetch (#2882)

--- apps/admin/lib/youtube.ts
+++ apps/admin/lib/youtube.ts
@@ -1,6 +1,6 @@
 import { youtube_v3 as youtube } from '@googleapis/youtube'
 
 export const youtubeClient = new youtube.Youtube({
-  auth: process.env.GOOGLE_API_KEY,
-  fetchImplementation: fetch
+  auth: process.env.GOOGLE_API_KEY
+  // fetchImplementation: fetch
 })
