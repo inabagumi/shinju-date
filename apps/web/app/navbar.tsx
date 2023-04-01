5b6dc44a0a392c616a7ea8a2acb13028a463a46d
Yamagishi Kazutoshi <ykzts@desire.sh>
2023-04-01 21:44:14 +0900

    Revert "build(deps): downgrade next.js to v13.2.4 (#2887)"
    This reverts commit 81c4a17beb8dee77f2ada3c1e0af61cebd6906d7.

--- apps/web/app/navbar.tsx
+++ apps/web/app/navbar.tsx
@@ -2,7 +2,7 @@
 
 import clsx from 'clsx'
 import Link from 'next/link'
-import { usePathname, useRouter } from 'next/navigation'
+import { useParams, usePathname, useRouter } from 'next/navigation'
 import { useTheme } from 'next-themes'
 import {
   type ChangeEventHandler,
@@ -19,12 +19,11 @@ import styles from './navbar.module.css'
 
 function SearchForm(): JSX.Element {
   const router = useRouter()
-  const pathname = usePathname()
-  const query = useMemo<string>(() => {
-    const m = pathname.match(/\/videos(?:\/(.*))?$/)
-
-    return m ? decodeURIComponent(m[1]) : ''
-  }, [pathname])
+  const params = useParams()
+  const query = useMemo<string>(
+    () => (params.queries ? decodeURIComponent(params.queries) : ''),
+    [params.queries]
+  )
   const [value, setValue] = useState(() => query)
   const textFieldRef = useRef<HTMLInputElement>(null)
 
