4f84abab353a6dee09a600288a134cc78f614392
Yamagishi Kazutoshi <ykzts@haneru.dev>
2023-04-08 22:07:50 +0900

    fix(admin): alway read response body (#2945)

--- apps/admin/lib/scraper/helpers.ts
+++ apps/admin/lib/scraper/helpers.ts
@@ -24,11 +24,15 @@ export function retryableFetch(
     async () => {
       const res = await fetch(input, init)
 
-      if (res.status === 404) {
-        throw new AbortError(res.statusText)
-      }
-
       if (!res.ok && res.status !== 304) {
+        // The body is read to reuse the socket.
+        // see https://github.com/nodejs/undici/issues/1203#issuecomment-1398191693
+        await res.arrayBuffer()
+
+        if (res.status === 404) {
+          throw new AbortError(res.statusText)
+        }
+
         throw new Error(res.statusText)
       }
 
