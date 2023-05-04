4ac9a6c3d6bc59f2796baef05ce8d95a3a9bfe2c
Yamagishi Kazutoshi <ykzts@haneru.dev>
2023-05-04 13:14:59 +0000

    feat(admin): remove dependencies of chakra-ui

--- apps/admin/app/(dashboard)/layout.tsx
+++ apps/admin/app/(dashboard)/layout.tsx
@@ -1,13 +1,4 @@
-import {
-  Box,
-  Flex,
-  Link,
-  SkipNavContent,
-  SkipNavLink
-} from '@shinju-date/chakra-ui'
-import NextLink from 'next/link'
 import { ReactNode } from 'react'
-import AuthButton from './auth-button'
 
 export type Props = {
   children: ReactNode
@@ -16,34 +7,7 @@ export type Props = {
 export default function DashboardLayout({ children }: Props): JSX.Element {
   return (
     <>
-      <SkipNavLink>コンテンツにスキップ</SkipNavLink>
-
-      <Box
-        as="header"
-        backdropBlur={15}
-        borderBottomWidth={1}
-        p={2}
-        pos="sticky"
-      >
-        <Flex justify="space-between">
-          <Flex alignItems="center">
-            <Link
-              _hover={{ textDecoration: 'none' }}
-              as={NextLink}
-              fontSize="2xl"
-              fontWeight="bold"
-              href="/"
-            >
-              Admin UI
-            </Link>
-          </Flex>
-          <Flex alignItems="center" px="4" py={2}>
-            <AuthButton />
-          </Flex>
-        </Flex>
-      </Box>
-      <SkipNavContent />
-      <Box as="main">{children}</Box>
+      <main>{children}</main>
     </>
   )
 }
--- apps/admin/app/provider.tsx
+++ apps/admin/app/provider.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import theme from '@shinju-date/chakra-theme'
-import { CacheProvider, ChakraProvider } from '@shinju-date/chakra-ui'
 import { type Session } from '@supabase/supabase-js'
 import { type ReactNode } from 'react'
 import { SessionProvider } from './session'
@@ -12,11 +10,5 @@ type Props = {
 }
 
 export default function Provider({ children, session }: Props) {
-  return (
-    <CacheProvider>
-      <ChakraProvider disableEnvironment theme={theme}>
-        <SessionProvider session={session}>{children}</SessionProvider>
-      </ChakraProvider>
-    </CacheProvider>
-  )
+  return <SessionProvider session={session}>{children}</SessionProvider>
 }
