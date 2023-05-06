4e2b69c24e35df67fd7b31f7b20032e3d62e290b
Yamagishi Kazutoshi <ykzts@haneru.dev>
2023-05-06 01:31:03 +0000

    feat: remove unused options

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,28 +1,10 @@
 import { withSentryConfig } from '@sentry/nextjs'
-import { fileURLToPath } from 'node:url'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    outputFileTracingExcludes: {
-      '*': [
-        './**/node_modules/@swc/core-linux-x64-gnu',
-        './**/node_modules/@swc/core-linux-x64-musl',
-        './**/node_modules/@esbuild/linux-x64',
-        './**/node_modules/webpack',
-        './**/node_modules/terser'
-      ]
-    },
-    outputFileTracingRoot: fileURLToPath(new URL('../..', import.meta.url))
-  }
+  reactStrictMode: true
 }
 
-export default withSentryConfig(
-  nextConfig,
-  {
-    silent: true
-  },
-  {
-    hideSourceMaps: true
-  }
-)
+export default withSentryConfig(nextConfig, {
+  silent: true
+})
