f7fbca35e34635336a1f2ae06ba0c71952c331c7
Yamagishi Kazutoshi <ykzts@haneru.dev>
2023-09-05 08:41:55 +0000

    fix(web): change svg attr from `stroke-width` to `strokeWidth`

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
                 cy="100.5"
                 r="52"
                 stroke="#FAFAFA"
-                stroke-width="21"
+                strokeWidth="21"
               />
               <path
                 d="M146.14 137l58.34 58.34a5 5 0 010 7.07l-7.07 7.07a5 5 0 01-7.07 0L132 151.14 146.14 137z"
