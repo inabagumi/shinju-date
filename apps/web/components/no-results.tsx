3ce38ad5ff799d70b8a49b2debc87e9a7320715b
Yamagishi Kazutoshi <ykzts@haneru.dev>
2023-09-06 01:58:41 +0900

    fix(web): remove unused `console.log` (#3471)

--- apps/web/components/no-results.tsx
+++ apps/web/components/no-results.tsx
@@ -2,19 +2,15 @@ import Link from 'next/link'
 import { joinURL } from 'ufo'
 import styles from './no-results.module.css'
 
-type Props = {
-  basePath?: string
-  message: string
-  title?: string
-}
-
 export default function NoResults({
   basePath = '/',
   message,
   title
-}: Props): JSX.Element {
-  console.log(basePath)
-
+}: {
+  basePath?: string
+  message: string
+  title?: string
+}) {
   return (
     <div className="text--center margin-bottom--lg margin-top--lg padding-bottom--lg padding-top--lg">
       {title && <h1 className={styles.title}>{title}</h1>}
