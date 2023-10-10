76e98598dcbca0066c1dfdfb50285f4d95e9f36b
Yamagishi Kazutoshi <ykzts@haneru.dev>
2023-10-10 11:45:29 +0900

    refactor(web): cleanup navbar (#3562)

--- apps/web/app/_components/navbar.tsx
+++ apps/web/app/_components/navbar.tsx
@@ -1,237 +1,261 @@
 'use client'
 
 import clsx from 'clsx'
-import Link from 'next/link'
-import { useParams, usePathname } from 'next/navigation'
+import Link, { type LinkProps } from 'next/link'
+import { usePathname } from 'next/navigation'
 import { useTheme } from 'next-themes'
 import {
-  type ChangeEventHandler,
+  type MouseEventHandler,
+  type ReactNode,
+  createContext,
   useCallback,
-  useMemo,
-  useRef,
+  useContext,
   useState
 } from 'react'
 import { FaMoon, FaSun } from 'react-icons/fa'
-import { search } from '@/app/_actions'
-import { title as siteName } from '@/lib/constants'
 import styles from './navbar.module.css'
 
-function SearchForm() {
-  const params = useParams()
-  const query = useMemo<string>(() => {
-    if (params.queries) {
-      const rawQuery = Array.isArray(params.queries)
-        ? params.queries.join('/')
-        : params.queries
-
-      return decodeURIComponent(rawQuery)
-    }
-
-    return ''
-  }, [params])
-  const [value, setValue] = useState(() => query)
-  const textFieldRef = useRef<HTMLInputElement>(null)
-
-  const handleChange = useCallback<ChangeEventHandler<HTMLInputElement>>(
-    (event) => {
-      setValue(event.target.value)
-    },
-    []
-  )
-
-  return (
-    <search className={styles.search}>
-      <form
-        // eslint-disable-next-line @typescript-eslint/no-misused-promises
-        action={async (formData) => {
-          textFieldRef.current?.blur()
-
-          await search(formData)
-        }}
-        className={clsx('navbar__search', styles.content)}
-      >
-        <input
-          aria-label="検索"
-          className={clsx('navbar__search-input', styles.input)}
-          name="q"
-          onChange={handleChange}
-          placeholder="検索"
-          ref={textFieldRef}
-          type="search"
-          value={value}
-        />
-      </form>
-    </search>
-  )
+type NavbarContextValue = {
+  setSidebarShown: (value: boolean) => void
+  sidebarShown: boolean
 }
 
-export default function Navbar() {
-  const [sidebarShown, setSidebarShown] = useState(false)
-  const pathname = usePathname()
+const NavbarContext = createContext<NavbarContextValue>({
+  setSidebarShown: () => {},
+  sidebarShown: false
+})
+
+function useNavbarContext() {
+  return useContext(NavbarContext)
+}
+
+export function ThemeToggleButton() {
   const { setTheme, theme } = useTheme()
-
-  const showSidebar = useCallback(() => {
-    setSidebarShown(true)
-  }, [])
-
-  const hideSidebar = useCallback(() => {
-    setSidebarShown(false)
-  }, [])
 
   const toggleTheme = useCallback(() => {
     setTheme(theme === 'dark' ? 'light' : 'dark')
   }, [setTheme, theme])
 
   return (
-    <nav
-      className={clsx('navbar', 'navbar--fixed-top', {
-        'navbar-sidebar--show': sidebarShown
-      })}
+    <button
+      aria-label={
+        theme === 'dark'
+          ? 'ライトモードに切り替える'
+          : 'ダークモードに切り替える'
+      }
+      className={clsx('navbar__item', 'navbar__link', styles.navbarButton)}
+      onClick={toggleTheme}
+      type="button"
     >
-      <div className="navbar__inner">
-        <div className="navbar__items">
-          <button
-            aria-label="メニューを開く"
-            className="clean-btn navbar__toggle"
-            onClick={showSidebar}
-            onKeyDown={showSidebar}
-            tabIndex={0}
-            type="button"
-          >
-            <svg
-              focusable="false"
-              height={30}
-              role="img"
-              viewBox="0 0 30 30"
-              width={30}
-            >
-              <path
-                d="M4 7h22M4 15h22M4 23h22"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeMiterlimit={10}
-                strokeWidth={2}
-              />
-            </svg>
-          </button>
+      {theme === 'dark' ? <FaSun /> : <FaMoon />}
+    </button>
+  )
+}
 
-          <Link className="navbar__brand" href="/">
-            <svg
-              className={clsx('navbar__logo', styles.logo)}
-              height={32}
-              role="img"
-              width={32}
-            >
-              <use xlinkHref="#svg-symbols-square-icon" />
-            </svg>
-            <strong className={clsx('navbar__title', styles.title)}>
-              {siteName}
-            </strong>
-          </Link>
+export function NavbarInner({ children }: { children: ReactNode }) {
+  return <div className="navbar__inner">{children}</div>
+}
 
-          <Link
-            aria-current={pathname === '/about' ? 'page' : undefined}
-            className={clsx('navbar__item', 'navbar__link', {
-              'navbar__link--active': pathname === '/about'
-            })}
-            href="/about"
-          >
-            {`${siteName}とは`}
-          </Link>
+export function NavbarItems({
+  children,
+  right = false
+}: {
+  children: ReactNode
+  right?: boolean
+}) {
+  return (
+    <div className={clsx('navbar__items', { 'navbar__items--right': right })}>
+      {children}
+    </div>
+  )
+}
 
-          <Link
-            aria-current={pathname.startsWith('/videos') ? 'page' : undefined}
-            className={clsx('navbar__item', 'navbar__link', {
-              'navbar__link--active': pathname.startsWith('/videos')
-            })}
-            href="/videos"
-          >
-            動画一覧
-          </Link>
-        </div>
+export function NavbarToggle() {
+  const { setSidebarShown } = useNavbarContext()
+  const showSidebar = useCallback(() => {
+    setSidebarShown(true)
+  }, [setSidebarShown])
 
-        <div className="navbar__items navbar__items--right">
-          <button
-            aria-label={
-              theme === 'dark'
-                ? 'ライトモードに切り替える'
-                : 'ダークモードに切り替える'
-            }
-            className={clsx(
-              'navbar__item',
-              'navbar__link',
-              styles.navbarButton
-            )}
-            onClick={toggleTheme}
-            type="button"
-          >
-            {theme === 'dark' ? <FaSun /> : <FaMoon />}
-          </button>
+  return (
+    <button
+      aria-label="メニューを開く"
+      className="clean-btn navbar__toggle"
+      onClick={showSidebar}
+      onKeyDown={showSidebar}
+      tabIndex={0}
+      type="button"
+    >
+      <svg
+        focusable="false"
+        height={30}
+        role="img"
+        viewBox="0 0 30 30"
+        width={30}
+      >
+        <path
+          d="M4 7h22M4 15h22M4 23h22"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          strokeWidth={2}
+        />
+      </svg>
+    </button>
+  )
+}
 
-          <SearchForm />
-        </div>
-      </div>
+export function NavbarBrand({
+  children,
+  ...props
+}: LinkProps & { children: ReactNode }) {
+  return (
+    <Link className="navbar__brand" {...props}>
+      <svg
+        className={clsx('navbar__logo', styles.logo)}
+        height={32}
+        role="img"
+        width={32}
+      >
+        <use xlinkHref="#svg-symbols-square-icon" />
+      </svg>
+      <strong className={clsx('navbar__title', styles.title)}>
+        {children}
+      </strong>
+    </Link>
+  )
+}
 
+export function NavbarItem({
+  href,
+  ...props
+}: LinkProps & { children: ReactNode }) {
+  const pathname = usePathname()
+  const url = typeof href === 'string' ? href : href.pathname
+  const isActive = pathname === url
+
+  return (
+    <Link
+      aria-current={isActive ? 'page' : undefined}
+      className={clsx('navbar__item', 'navbar__link', {
+        'navbar__link--active': isActive
+      })}
+      href={href}
+      {...props}
+    />
+  )
+}
+
+export function NavbarSidebar({ children }: { children: ReactNode }) {
+  const { setSidebarShown } = useNavbarContext()
+  const hideSidebar = useCallback(() => {
+    setSidebarShown(false)
+  }, [setSidebarShown])
+
+  return (
+    <>
       <div
         className="navbar-sidebar__backdrop"
         onClick={hideSidebar}
         role="presentation"
       />
 
-      <div className="navbar-sidebar">
-        <div className="navbar-sidebar__brand">
-          <Link className="navbar__brand" href="/" onClick={hideSidebar}>
-            <svg className="navbar__logo" height={32} role="img" width={32}>
-              <use xlinkHref="#svg-symbols-square-icon" />
-            </svg>
-            <strong className="navbar__title">{siteName}</strong>
-          </Link>
-        </div>
-        <div className="navbar-sidebar__items">
-          <div className="menu navbar-sidebar__item">
-            <ul className="menu__list">
-              <li className="menu__list-item">
-                <Link
-                  aria-current={pathname === '/' ? 'page' : undefined}
-                  className={clsx('menu__link', {
-                    'menu__link--active': pathname === '/'
-                  })}
-                  href="/"
-                  onClick={hideSidebar}
-                >
-                  配信予定
-                </Link>
-              </li>
-              <li className="menu__list-item">
-                <Link
-                  aria-current={
-                    pathname.startsWith('/videos') ? 'page' : undefined
-                  }
-                  className={clsx('menu__link', {
-                    'menu__link--active': pathname.startsWith('/videos')
-                  })}
-                  href="/videos"
-                  onClick={hideSidebar}
-                >
-                  動画一覧
-                </Link>
-              </li>
-              <li className="menu__list-item">
-                <Link
-                  aria-current={pathname === '/about' ? 'page' : undefined}
-                  className={clsx('menu__link', {
-                    'menu__link--active': pathname === '/about'
-                  })}
-                  href="/about"
-                  onClick={hideSidebar}
-                >
-                  {`${siteName}とは`}
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
+      <div className="navbar-sidebar">{children}</div>
+    </>
+  )
+}
+
+export function NavbarSidebarBrand({
+  onClick,
+  ...props
+}: LinkProps & { children: ReactNode }) {
+  const { setSidebarShown } = useNavbarContext()
+  const hideSidebar = useCallback(() => {
+    setSidebarShown(false)
+  }, [setSidebarShown])
+
+  const handleClick = useCallback<MouseEventHandler<HTMLAnchorElement>>(
+    (event) => {
+      if (typeof onClick === 'function') {
+        onClick(event)
+      }
+
+      hideSidebar()
+    },
+    [hideSidebar, onClick]
+  )
+
+  return (
+    <div className="navbar-sidebar__brand">
+      <NavbarBrand onClick={handleClick} {...props} />
+    </div>
+  )
+}
+
+export function NavbarSidebarMenu({ children }: { children: ReactNode }) {
+  return (
+    <div className="navbar-sidebar__items">
+      <div className="menu navbar-sidebar__item">
+        <ul className="menu__list">{children}</ul>
       </div>
-    </nav>
+    </div>
+  )
+}
+
+export function NavbarSidebarMenuItem({
+  children,
+  href,
+  onClick,
+  ...props
+}: LinkProps & { children: ReactNode }) {
+  const pathname = usePathname()
+  const { setSidebarShown } = useNavbarContext()
+  const hideSidebar = useCallback(() => {
+    setSidebarShown(false)
+  }, [setSidebarShown])
+
+  const handleClick = useCallback<MouseEventHandler<HTMLAnchorElement>>(
+    (event) => {
+      if (typeof onClick === 'function') {
+        onClick(event)
+      }
+
+      hideSidebar()
+    },
+    [hideSidebar, onClick]
+  )
+
+  const url = typeof href === 'string' ? href : href.pathname
+  const isActive = pathname === url
+
+  return (
+    <li className="menu__list-item">
+      <Link
+        aria-current={isActive ? 'page' : undefined}
+        className={clsx('menu__link', {
+          'menu__link--active': isActive
+        })}
+        href="/"
+        onClick={handleClick}
+        {...props}
+      >
+        {children}
+      </Link>
+    </li>
+  )
+}
+
+export default function Navbar({ children }: { children: ReactNode }) {
+  const [sidebarShown, setSidebarShown] = useState(false)
+
+  return (
+    <NavbarContext.Provider value={{ setSidebarShown, sidebarShown }}>
+      <nav
+        className={clsx('navbar', 'navbar--fixed-top', {
+          'navbar-sidebar--show': sidebarShown
+        })}
+      >
+        {children}
+      </nav>
+    </NavbarContext.Provider>
   )
 }
