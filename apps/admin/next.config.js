c2763113f5076a2922c1f022784f3dfeb2708bbb
Yamagishi Kazutoshi <ykzts@haneru.dev>
2024-01-08 00:55:34 +0000

    build(sentry): enable profiling

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -2,6 +2,9 @@ import { withSentryConfig } from '@sentry/nextjs'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@sentry/profiling-node']
+  },
   reactStrictMode: true
 }
 
