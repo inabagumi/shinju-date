5834945a889405ec8ecc5973868e2ba21c58c22b
Yamagishi Kazutoshi <ykzts@haneru.dev>
2024-01-09 17:39:52 +0000

    build(sentry): disable sentry on not prod env

--- apps/admin/sentry.edge.config.ts
+++ apps/admin/sentry.edge.config.ts
@@ -2,5 +2,6 @@ import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  enabled: process.env.VERCEL_ENV === 'production',
   tracesSampleRate: 1.0
 })
--- apps/admin/sentry.server.config.ts
+++ apps/admin/sentry.server.config.ts
@@ -3,6 +3,7 @@ import { ProfilingIntegration } from '@sentry/profiling-node'
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  enabled: process.env.VERCEL_ENV === 'production',
   ignoreErrors: ['AbortError'],
   integrations: [new ProfilingIntegration()],
   profilesSampleRate: 1.0,
