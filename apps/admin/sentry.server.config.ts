8fc5e3773444f86bdd9331854ff0fb44261a1773
Yamagishi Kazutoshi <ykzts@haneru.dev>
2024-01-10 00:29:31 +0900

    fix(admin): ignore `AbortError`

--- apps/admin/sentry.server.config.ts
+++ apps/admin/sentry.server.config.ts
@@ -3,6 +3,7 @@ import { ProfilingIntegration } from '@sentry/profiling-node'
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  ignoreErrors: ['AbortError'],
   integrations: [new ProfilingIntegration()],
   profilesSampleRate: 1.0,
   tracesSampleRate: 1.0
