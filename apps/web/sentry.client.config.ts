7bdfed821ac83fe19e0a993c84bf873b32941bfa
Yamagishi Kazutoshi <ykzts@haneru.dev>
2024-01-10 02:45:23 +0900

    build(sentry): disable sentry on not prod env (#3812)

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -2,6 +2,7 @@ import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  enabled: process.env.VERCEL_ENV === 'production',
   integrations: [new Sentry.BrowserProfilingIntegration()],
   profilesSampleRate: 1.0,
   tracesSampleRate: 1.0
