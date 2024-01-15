167fca90ea465eb93c86e5cc734fbd7fe437293c
Yamagishi Kazutoshi <ykzts@haneru.dev>
2024-01-15 13:11:09 +0000

    build(sentry): enable replay

--- apps/admin/sentry.client.config.ts
+++ apps/admin/sentry.client.config.ts
@@ -3,7 +3,9 @@ import * as Sentry from '@sentry/nextjs'
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   enabled: process.env.VERCEL_ENV === 'production',
-  integrations: [new Sentry.BrowserProfilingIntegration()],
+  integrations: [new Sentry.BrowserProfilingIntegration(), new Sentry.Replay()],
   profilesSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
   tracesSampleRate: 1.0
 })
