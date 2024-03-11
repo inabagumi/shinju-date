5cee8501e45668324930be5eb55150d1f9315cff
Yamagishi Kazutoshi <ykzts@haneru.dev>
2024-03-12 00:46:09 +0900

    build(prettier): change to esm

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,1 +1,8 @@
-module.exports = require('@inabagumi/prettier-config')
+import inabagumiPrettierConfig from '@inabagumi/prettier-config'
+
+/** @type {import('prettier').Config} */
+const prettierConfig = {
+  ...inabagumiPrettierConfig
+}
+
+export default prettierConfig
