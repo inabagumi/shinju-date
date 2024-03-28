9984d6c099e0b336a551553e56ac73f34c6d8d0b
Yamagishi Kazutoshi <ykzts@haneru.dev>
2024-03-28 13:01:23 +0900

    build(prettier): add tailwind to root deps

--- prettier.config.js
+++ prettier.config.js
@@ -1,13 +1,10 @@
-import { fileURLToPath } from 'node:url'
 import inabagumiPrettierConfig from '@inabagumi/prettier-config'
 
 /** @type {import('prettier').Config} */
 const prettierConfig = {
   ...inabagumiPrettierConfig,
   plugins: ['prettier-plugin-tailwindcss'],
-  tailwindEntryPoint: fileURLToPath(
-    new URL('./apps/admin/app/globals.css', import.meta.url)
-  )
+  tailwindEntryPoint: './apps/admin/app/globals.css'
 }
 
 export default prettierConfig
