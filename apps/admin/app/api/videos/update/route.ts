39368da6021d3235dc7b7decf10c9eb9e3d140eb
Yamagishi Kazutoshi <ykzts@haneru.dev>
2024-03-31 10:50:41 +0000

    feat(admin): revalidate on cron jobs

--- apps/admin/app/api/videos/update/route.ts
+++ apps/admin/app/api/videos/update/route.ts
@@ -4,6 +4,7 @@ import PQueue from 'p-queue'
 import { Temporal } from 'temporal-polyfill'
 import { captureException, defaultLogger as logger } from '@/lib/logging'
 import { videosUpdate as ratelimit } from '@/lib/ratelimit'
+import { revalidateTags } from '@/lib/revalidate'
 import { type Video, scrape } from '@/lib/scraper'
 import { type FilteredYouTubeChannel, getChannels } from '@/lib/youtube'
 
@@ -108,6 +109,8 @@ export async function POST(request: Request): Promise<Response> {
       })
     }
   }
+
+  await revalidateTags(['videos'])
 
   return new Response(null, {
     status: 204
