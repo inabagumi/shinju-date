6391fdf689240086166b8d88be61e29d760feb4e
Yamagishi Kazutoshi <ykzts@haneru.dev>
2024-04-02 12:47:16 +0000

    feat(web): add suffix to click event key

--- apps/web/app/api/ping/_lib/increment.ts
+++ apps/web/app/api/ping/_lib/increment.ts
@@ -1,11 +1,25 @@
+import { Temporal } from 'temporal-polyfill'
+import { timeZone } from '@/lib/constants'
 import { redisClient } from '@/lib/redis'
 import { type Video } from './types'
 
-export default async function increment(video: Video): Promise<void> {
+type IncrementOptions = {
+  timestamp?: Temporal.ZonedDateTime
+}
+
+export default async function increment(
+  video: Video,
+  { timestamp = Temporal.Now.zonedDateTimeISO(timeZone) }: IncrementOptions = {}
+): Promise<void> {
+  const keySuffix = [
+    timestamp.year.toString(10).padStart(4, '0'),
+    timestamp.month.toString(10).padStart(2, '0'),
+    timestamp.day.toString(10).padStart(2, '0')
+  ].join('')
   const multi = redisClient.multi()
 
-  multi.zincrby('videos:clicked', 1, video.id)
-  multi.zincrby('channels:clicked', 1, video.channel.id)
+  multi.zincrby(`videos:clicked:${keySuffix}`, 1, video.id)
+  multi.zincrby(`channels:clicked:${keySuffix}`, 1, video.channel.id)
 
   await multi.exec()
 }
