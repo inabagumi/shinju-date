ced93442d72451a392c048d33fcda3b80ad9b4a9
Yamagishi Kazutoshi <ykzts@haneru.dev>
2024-06-09 07:01:41 +0000

    fix(batch): replace to `after`

--- apps/batch/app/channels/update/route.ts
+++ apps/batch/app/channels/update/route.ts
@@ -2,12 +2,15 @@ import * as Sentry from '@sentry/nextjs'
 import { type Tables } from '@shinju-date/database'
 import { createErrorResponse, verifyCronRequest } from '@shinju-date/helpers'
 import { defaultLogger as logger } from '@shinju-date/logging'
+import { unstable_after as after } from 'next/server'
 import { Temporal } from 'temporal-polyfill'
 import { channelsUpdate as ratelimit } from '@/lib/ratelimit'
 import { revalidateTags } from '@/lib/revalidate'
 import { captureException } from '@/lib/sentry'
 import { supabaseClient } from '@/lib/supabase'
 import { youtubeClient } from '@/lib/youtube'
+
+const MONITOR_SLUG = '/channels/update'
 
 export const runtime = 'nodejs'
 export const revalidate = 0
@@ -30,116 +33,122 @@ export async function POST(request: Request): Promise<Response> {
     )
   }
 
+  const currentDateTime = Temporal.Now.instant()
+  const checkInId = Sentry.captureCheckIn(
+    {
+      monitorSlug: MONITOR_SLUG,
+      status: 'in_progress'
+    },
+    {
+      schedule: {
+        type: 'crontab',
+        value: '21 1/3 * * *'
+      },
+      timezone: 'Etc/UTC'
+    }
+  )
+
   try {
-    await Sentry.withMonitor(
-      '/channels/update',
-      async () => {
-        const currentDateTime = Temporal.Now.instant()
-        const { data: channels, error } = await supabaseClient
+    const { data: channels, error } = await supabaseClient
+      .from('channels')
+      .select('name, slug')
+      .is('deleted_at', null)
+
+    if (error) {
+      throw new TypeError(error.message, { cause: error })
+    }
+
+    const {
+      data: { items = [] }
+    } = await youtubeClient.channels.list({
+      id: channels.map((channel) => channel.slug),
+      maxResults: channels.length,
+      part: ['snippet']
+    })
+
+    const results = await Promise.allSettled(
+      items.map<Promise<Channel | null>>(async (item) => {
+        if (!item.id) {
+          throw new TypeError('The ID not found.')
+        }
+
+        const channel = channels.find((channel) => channel.slug === item.id)
+
+        if (!channel) {
+          throw new TypeError('A channel does not exist.')
+        }
+
+        if (!item.snippet || !item.snippet.title) {
+          throw new TypeError('A snippet is empty.')
+        }
+
+        if (item.snippet.title === channel.name) {
+          return null
+        }
+
+        const { data, error } = await supabaseClient
           .from('channels')
+          .update({
+            name: item.snippet.title,
+            updated_at: currentDateTime.toJSON()
+          })
+          .eq('slug', item.id)
           .select('name, slug')
-          .is('deleted_at', null)
+          .single()
 
         if (error) {
           throw new TypeError(error.message, { cause: error })
         }
 
-        const {
-          data: { items = [] }
-        } = await youtubeClient.channels.list({
-          id: channels.map((channel) => channel.slug),
-          maxResults: channels.length,
-          part: ['snippet']
-        })
-
-        const results = await Promise.allSettled(
-          items.map<Promise<Channel | null>>(async (item) => {
-            if (!item.id) {
-              throw new TypeError('The ID not found.')
-            }
-
-            const channel = channels.find((channel) => channel.slug === item.id)
-
-            if (!channel) {
-              throw new TypeError('A channel does not exist.')
-            }
-
-            if (!item.snippet || !item.snippet.title) {
-              throw new TypeError('A snippet is empty.')
-            }
-
-            if (item.snippet.title === channel.name) {
-              return null
-            }
-
-            const { data, error } = await supabaseClient
-              .from('channels')
-              .update({
-                name: item.snippet.title,
-                updated_at: currentDateTime.toJSON()
-              })
-              .eq('slug', item.id)
-              .select('name, slug')
-              .single()
-
-            if (error) {
-              throw new TypeError(error.message, { cause: error })
-            }
-
-            return data
-          })
-        )
-
-        let isUpdated = false
-
-        for (const result of results) {
-          if (result.status === 'fulfilled' && result.value) {
-            const newChannel = result.value
-            const channelID = newChannel.slug
-            const channel = channels.find(
-              (channel) => channel.slug === channelID
-            )
-
-            if (!channel) {
-              continue
-            }
-
-            const changedColumns: Partial<typeof channel> = {}
-
-            if (channel.name !== newChannel.name) {
-              changedColumns.name = `${channel.name} -> ${newChannel.name}`
-            }
-
-            logger.info('Channel information has been updated.', changedColumns)
-
-            if (!isUpdated) {
-              isUpdated = true
-            }
-          } else if (result.status === 'rejected') {
-            captureException(result.reason)
-          }
-        }
-
-        if (isUpdated) {
-          await revalidateTags(['channels'], {
-            signal: request.signal
-          })
-        }
-      },
-      {
-        schedule: {
-          type: 'crontab',
-          value: '21 1/3 * * *'
-        },
-        timezone: 'Etc/UTC'
-      }
+        return data
+      })
     )
-  } catch (error) {
-    captureException(error)
 
-    // wait for logging
-    await new Promise((resolve) => {
-      setTimeout(resolve, 2_000)
+    let isUpdated = false
+
+    for (const result of results) {
+      if (result.status === 'fulfilled' && result.value) {
+        const newChannel = result.value
+        const channelID = newChannel.slug
+        const channel = channels.find((channel) => channel.slug === channelID)
+
+        if (!channel) {
+          continue
+        }
+
+        const changedColumns: Partial<typeof channel> = {}
+
+        if (channel.name !== newChannel.name) {
+          changedColumns.name = `${channel.name} -> ${newChannel.name}`
+        }
+
+        logger.info('Channel information has been updated.', changedColumns)
+
+        if (!isUpdated) {
+          isUpdated = true
+        }
+      } else if (result.status === 'rejected') {
+        after(() => {
+          captureException(result.reason)
+        })
+      }
+    }
+
+    if (isUpdated) {
+      await revalidateTags(['channels'], {
+        signal: request.signal
+      })
+    }
+  } catch (error) {
+    after(() => {
+      captureException(error)
+
+      Sentry.captureCheckIn({
+        checkInId,
+        duration: currentDateTime.until(Temporal.Now.instant()).seconds,
+        monitorSlug: MONITOR_SLUG,
+        status: 'error'
+      })
     })
 
     const message =
@@ -148,9 +157,13 @@ export async function POST(request: Request): Promise<Response> {
     return createErrorResponse(message, { status: 500 })
   }
 
-  // wait for logging
-  await new Promise((resolve) => {
-    setTimeout(resolve, 2_000)
+  after(() => {
+    Sentry.captureCheckIn({
+      checkInId,
+      duration: currentDateTime.until(Temporal.Now.instant()).seconds,
+      monitorSlug: MONITOR_SLUG,
+      status: 'ok'
+    })
   })
 
   return new Response(null, {
--- apps/batch/app/videos/update/route.ts
+++ apps/batch/app/videos/update/route.ts
@@ -1,6 +1,7 @@
 import * as Sentry from '@sentry/nextjs'
 import { createErrorResponse, verifyCronRequest } from '@shinju-date/helpers'
 import { defaultLogger as logger } from '@shinju-date/logging'
+import { unstable_after as after } from 'next/server'
 import PQueue from 'p-queue'
 import { Temporal } from 'temporal-polyfill'
 import { videosUpdate as ratelimit } from '@/lib/ratelimit'
@@ -9,6 +10,8 @@ import { type Video, scrape } from '@/lib/scraper'
 import { captureException } from '@/lib/sentry'
 import { supabaseClient } from '@/lib/supabase'
 import { type FilteredYouTubeChannel, getChannels } from '@/lib/youtube'
+
+const MONITOR_SLUG = '/videos/update'
 
 export const runtime = 'nodejs'
 export const revalidate = 0
@@ -29,103 +32,108 @@ export async function POST(request: Request): Promise<Response> {
     )
   }
 
-  try {
-    await Sentry.withMonitor(
-      '/videos/update',
-      async () => {
-        const currentDateTime = Temporal.Now.instant()
-
-        const { data: savedChannels, error } = await supabaseClient
-          .from('channels')
-          .select('id, slug')
-          .is('deleted_at', null)
-
-        if (error) {
-          throw new TypeError(error.message, { cause: error })
-        }
-
-        const channelIDs = savedChannels.map(
-          (savedChannel) => savedChannel.slug
-        )
-        const channels: FilteredYouTubeChannel[] = []
-
-        for await (const channel of getChannels({ ids: channelIDs })) {
-          channels.push(channel)
-        }
-
-        if (channels.length < 1) {
-          throw new TypeError('There are no channels.')
-        }
-
-        const queue = new PQueue({
-          concurrency: 1,
-          interval: 250
-        })
-
-        const results = await Promise.allSettled(
-          savedChannels.map((savedChannel) => {
-            const originalChannel = channels.find(
-              (item) => item.id === savedChannel.slug
-            )
-
-            if (!originalChannel) {
-              return Promise.reject(new TypeError('Channel does not exist.'))
-            }
-
-            return queue.add(() =>
-              scrape({
-                channel: originalChannel,
-                currentDateTime,
-                savedChannel: savedChannel,
-                supabaseClient
-              })
-            )
-          })
-        )
-
-        const videos: Video[] = []
-
-        for (const result of results) {
-          if (result.status === 'fulfilled' && result.value) {
-            for (const video of result.value) {
-              videos.push(video)
-            }
-          } else if (result.status === 'rejected') {
-            captureException(result.reason)
-          }
-        }
-
-        if (videos.length > 0) {
-          for (const video of videos) {
-            const publishedAt = Temporal.Instant.from(video.published_at)
-
-            logger.info('The video has been saved.', {
-              duration: video.duration,
-              id: video.slug,
-              publishedAt: publishedAt.toString(),
-              title: video.title
-            })
-          }
-
-          await revalidateTags(['videos'], {
-            signal: request.signal
-          })
-        }
+  const currentDateTime = Temporal.Now.instant()
+  const checkInId = Sentry.captureCheckIn(
+    {
+      monitorSlug: MONITOR_SLUG,
+      status: 'in_progress'
+    },
+    {
+      schedule: {
+        type: 'crontab',
+        value: '1/10 * * * *'
       },
-      {
-        schedule: {
-          type: 'crontab',
-          value: '1/10 * * * *'
-        },
-        timezone: 'Etc/UTC'
-      }
-    )
-  } catch (error) {
-    captureException(error)
+      timezone: 'Etc/UTC'
+    }
+  )
 
-    // wait for logging
-    await new Promise((resolve) => {
-      setTimeout(resolve, 2_000)
+  try {
+    const { data: savedChannels, error } = await supabaseClient
+      .from('channels')
+      .select('id, slug')
+      .is('deleted_at', null)
+
+    if (error) {
+      throw new TypeError(error.message, { cause: error })
+    }
+
+    const channelIDs = savedChannels.map((savedChannel) => savedChannel.slug)
+    const channels: FilteredYouTubeChannel[] = []
+
+    for await (const channel of getChannels({ ids: channelIDs })) {
+      channels.push(channel)
+    }
+
+    if (channels.length < 1) {
+      throw new TypeError('There are no channels.')
+    }
+
+    const queue = new PQueue({
+      concurrency: 1,
+      interval: 250
+    })
+
+    const results = await Promise.allSettled(
+      savedChannels.map((savedChannel) => {
+        const originalChannel = channels.find(
+          (item) => item.id === savedChannel.slug
+        )
+
+        if (!originalChannel) {
+          return Promise.reject(new TypeError('Channel does not exist.'))
+        }
+
+        return queue.add(() =>
+          scrape({
+            channel: originalChannel,
+            currentDateTime,
+            savedChannel: savedChannel,
+            supabaseClient
+          })
+        )
+      })
+    )
+
+    const videos: Video[] = []
+
+    for (const result of results) {
+      if (result.status === 'fulfilled' && result.value) {
+        for (const video of result.value) {
+          videos.push(video)
+        }
+      } else if (result.status === 'rejected') {
+        after(() => {
+          captureException(result.reason)
+        })
+      }
+    }
+
+    if (videos.length > 0) {
+      for (const video of videos) {
+        const publishedAt = Temporal.Instant.from(video.published_at)
+
+        logger.info('The video has been saved.', {
+          duration: video.duration,
+          id: video.slug,
+          publishedAt: publishedAt.toString(),
+          title: video.title
+        })
+      }
+
+      await revalidateTags(['videos'], {
+        signal: request.signal
+      })
+    }
+  } catch (error) {
+    after(() => {
+      captureException(error)
+
+      Sentry.captureCheckIn({
+        checkInId,
+        duration: currentDateTime.until(Temporal.Now.instant()).seconds,
+        monitorSlug: MONITOR_SLUG,
+        status: 'error'
+      })
     })
 
     const message =
@@ -134,9 +142,13 @@ export async function POST(request: Request): Promise<Response> {
     return createErrorResponse(message, { status: 500 })
   }
 
-  // wait for logging
-  await new Promise((resolve) => {
-    setTimeout(resolve, 2_000)
+  after(() => {
+    Sentry.captureCheckIn({
+      checkInId,
+      duration: currentDateTime.until(Temporal.Now.instant()).seconds,
+      monitorSlug: MONITOR_SLUG,
+      status: 'ok'
+    })
   })
 
   return new Response(null, {
