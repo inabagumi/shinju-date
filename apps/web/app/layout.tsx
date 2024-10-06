50efce385d1bd1878a505456c9e76eaaea9ca3dd
Yamagishi Kazutoshi <ykzts@haneru.dev>
2024-10-06 15:22:24 +0000

    fix(web): remove speed insights

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,5 @@
 import './globals.css'
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 import { type Metadata, type Viewport } from 'next'
 import Link from 'next/link'
 import { type ReactNode } from 'react'
@@ -120,7 +119,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         </footer>
 
         <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   )
