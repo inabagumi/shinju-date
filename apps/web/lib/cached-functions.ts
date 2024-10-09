1dc7b808762bc0193817ed447c6ce01c9d7c8e84
Yamagishi Kazutoshi <ykzts@haneru.dev>
2024-10-09 14:55:30 +0000

    build(deps): downgrade next to 15.0.0-canary.179

--- apps/web/lib/cached-functions.ts
+++ apps/web/lib/cached-functions.ts
@@ -2,7 +2,7 @@ import { Temporal } from 'temporal-polyfill'
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export async function getCurrentTime(): Promise<bigint> {
-  'use cache'
+  // 'use cache'
 
   return Temporal.Now.instant().epochNanoseconds
 }
