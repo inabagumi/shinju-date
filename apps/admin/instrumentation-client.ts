bdc15af64ebc7354c5ae9072a239bcba24fc1576
Yamagishi Kazutoshi <ykzts@haneru.dev>
2025-05-06 00:39:35 +0900

    fix: use dynamic import (#5007)

--- apps/admin/instrumentation-client.ts
+++ apps/admin/instrumentation-client.ts
@@ -1,21 +1,36 @@
-import * as Sentry from '@sentry/nextjs'
-
 const dsn = process.env['NEXT_PUBLIC_SENTRY_DSN']
 
 if (dsn) {
-  Sentry.init({
-    dsn,
-    enabled: process.env['VERCEL_ENV'] === 'production',
-    integrations: [
-      Sentry.browserTracingIntegration(),
-      Sentry.browserProfilingIntegration(),
-      Sentry.replayIntegration()
-    ],
-    profilesSampleRate: 1.0,
-    replaysOnErrorSampleRate: 1.0,
-    replaysSessionSampleRate: 0.1,
-    tracesSampleRate: 1.0
-  })
+  import('@sentry/nextjs')
+    .then((Sentry) => {
+      Sentry.init({
+        dsn,
+        enabled: process.env['VERCEL_ENV'] === 'production',
+        integrations: [
+          Sentry.browserTracingIntegration(),
+          Sentry.browserProfilingIntegration(),
+          Sentry.replayIntegration()
+        ],
+        profilesSampleRate: 1.0,
+        replaysOnErrorSampleRate: 1.0,
+        replaysSessionSampleRate: 0.1,
+        tracesSampleRate: 1.0
+      })
+    })
+    .catch((error) => {
+      throw error
+    })
 }
 
-export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
+export async function onRouterTransitionStart(
+  href: string,
+  navigationType: string
+) {
+  const dsn = process.env['NEXT_PUBLIC_SENTRY_DSN']
+
+  if (dsn) {
+    const { captureRouterTransitionStart } = await import('@sentry/nextjs')
+
+    captureRouterTransitionStart(href, navigationType)
+  }
+}
