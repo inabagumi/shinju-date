5a03c722212ac0072811da279b06ffb5a4e67ba9
Yamagishi Kazutoshi <ykzts@haneru.dev>
2025-10-17 09:08:36 +0000

    refactor(supabase): simplify createSupabaseClient function and improve type definitions

--- apps/batch/lib/supabase.ts
+++ apps/batch/lib/supabase.ts
@@ -1,33 +1,20 @@
 import type { default as DefaultDatabase } from '@shinju-date/database'
 import retryableFetch from '@shinju-date/retryable-fetch'
-import {
-  createClient,
-  type SupabaseClient,
-  type SupabaseClientOptions,
-} from '@supabase/supabase-js'
+import { createClient, type SupabaseClient } from '@supabase/supabase-js'
 
-export type TypedSupabaseClient<
-  Database = DefaultDatabase,
-  SchemaName extends string & keyof Database = 'public' extends keyof Database
-    ? 'public'
-    : string & keyof Database,
-> = SupabaseClient<Database, SchemaName>
+export type TypedSupabaseClient<Database = DefaultDatabase> =
+  SupabaseClient<Database>
 
-export function createSupabaseClient<
-  Database = DefaultDatabase,
-  SchemaName extends string & keyof Database = 'public' extends keyof Database
-    ? 'public'
-    : string & keyof Database,
->(
+export function createSupabaseClient<Database = DefaultDatabase>(
   url: string | undefined = process.env['NEXT_PUBLIC_SUPABASE_URL'],
   key: string | undefined = process.env['SUPABASE_SERVICE_ROLE_KEY'],
-  options?: SupabaseClientOptions<SchemaName>,
-): SupabaseClient<Database, SchemaName> {
+  options?: NonNullable<Parameters<typeof createClient<Database>>[2]>,
+): SupabaseClient<Database> {
   if (!url || !key) {
     throw new TypeError('Supabase URL and key are required.')
   }
 
-  return createClient<Database, SchemaName>(url, key, {
+  return createClient<Database>(url, key, {
     ...options,
     global: {
       ...options?.global,
